db750870234132a5877936899c9c5da7df505415
Ludivine Coipel <coipel.ludivine@gmail.com>
2018-01-10 21:35:17 +0100

    update gem decidim

---
+++ db/migrate/20180110203201_add_target_to_navbar_link.decidim.rb
@@ -0,0 +1,6 @@
+# This migration comes from decidim (originally 20180110144519)
+class AddTargetToNavbarLink < ActiveRecord::Migration[5.1]
+  def change
+    add_column :decidim_navbar_links, :target, :string
+  end
+end
