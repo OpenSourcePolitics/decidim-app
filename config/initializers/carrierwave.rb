22cf55f0563a4aed424e775ebb1620d2083f8bad
moustachu <git@moustachu.net>
2018-01-23 13:38:14 +0100

    enabling S3 upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,19 +10,18 @@ CarrierWave.configure do |config|
 end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
-#
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'                                             # required
-#   config.fog_credentials = {
-#     provider:              'AWS',                                             # required
-#     aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
-#     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-#     region:                'eu-west-1',                                       # optional, defaults to 'us-east-1'
-#     host:                  's3.example.com',                                  # optional, defaults to nil
-#     endpoint:              'https://s3.example.com:8080'                      # optional, defaults to nil
-#   }
-#   config.fog_directory  = 'name_of_directory'                                 # required
-#   config.fog_public     = false                                               # optional, defaults to true
-#   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
-# end
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'                                             # required
+  config.fog_credentials = {
+    provider:              'AWS',                                             # required
+    aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+    region:                'eu-central-1',                                    # optional, defaults to 'us-east-1'
+    host:                  's3.eu-central-1.amazonaws.com',                    # optional, defaults to nil
+  }
+  config.fog_directory  = 'osp-app'                                 # required
+  config.fog_public     = true                                               # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
+end
