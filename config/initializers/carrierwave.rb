9134d2fd1bb0cb4fd80be52cc5d39f581b8693c3
moustachu <git@moustachu.net>
2018-01-23 13:42:24 +0100

    fixing S3 upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,17 +11,19 @@ end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'                                             # required
-  config.fog_credentials = {
-    provider:              'AWS',                                             # required
-    aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-    region:                'eu-central-1',                                    # optional, defaults to 'us-east-1'
-    host:                  's3.eu-central-1.amazonaws.com',                    # optional, defaults to nil
-  }
-  config.fog_directory  = 'osp-app'                                 # required
-  config.fog_public     = true                                               # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                                             # required
+    config.fog_credentials = {
+      provider:              'AWS',                                             # required
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+      region:                'eu-central-1',                                    # optional, defaults to 'us-east-1'
+      host:                  's3.eu-central-1.amazonaws.com',                                  # optional, defaults to nil
+    }
+    config.fog_directory  = 'decidim-heroku'                                 # required
+    config.fog_public     = true                                               # optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }    # optional, defaults to {}
+    config.storage = :fog
+  end
 end
