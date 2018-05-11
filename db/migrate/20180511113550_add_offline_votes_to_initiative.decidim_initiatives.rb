4856f29bf82d1bef3d8f4ae522d8bd37f6cfe024
Julie Simon <juliesimon.js@gmail.com>
2018-05-11 14:00:21 +0200

    upgrade to 0.11-stable

---
+++ db/migrate/20180511113550_add_offline_votes_to_initiative.decidim_initiatives.rb
@@ -0,0 +1,9 @@
+# This migration comes from decidim_initiatives (originally 20171031183855)
+# frozen_string_literal: true
+
+class AddOfflineVotesToInitiative < ActiveRecord::Migration[5.1]
+  def change
+    add_column :decidim_initiatives,
+               :offline_votes, :integer
+  end
+end
