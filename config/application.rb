d610b6fe51046d8068a7577ef42088b8714367ee
moustachu <git@moustachu.net>
2018-07-09 00:48:00 +0200

    fix TImezone

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,11 @@ module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = "Paris"
+
+    if ENV["ASSET_HOST"].present?
+      config.action_mailer.asset_host = ENV['ASSET_HOST']
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
