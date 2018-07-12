c70851aee53674e322e62f7b089fd5e34f2a41e2
moustachu <git@moustachu.net>
2018-07-12 14:18:13 +0200

    fix for Initiatives config

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -17,7 +17,11 @@ Decidim.configure do |config|
     here_app_code: Rails.application.secrets.geocoder[:here_app_code]
   }
 
-  Decidim::Initiatives.do_not_require_authorization = true
+  if defined?(Decidim::Initiatives)
+    # puts "Decidim::Initiatives are loaded"
+    Decidim::Initiatives.do_not_require_authorization = true
+  end
+
 
   # Custom resource reference generator method
   # config.resource_reference_generator = lambda do |resource, feature|
