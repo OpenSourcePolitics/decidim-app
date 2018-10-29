73478764b7cbdeceb1c6894115a607c135749b31
moustachu <git@moustachu.net>
2018-10-29 12:15:55 +0100

    :bug: :wrench: harmony Uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,6 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(:harmony => true)
-  config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
