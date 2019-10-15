60a0b5e9a7d3d223eb78d681f343abe1441044a3
Armand <fardeauarmand@gmail.com>
2019-10-15 18:41:43 +0200

    Add Europe/Paris to timezone

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.time_zone = "Paris"
+    config.time_zone = "Europe/Paris"
 
     # This needs to be set for correct images URLs in emails
     # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
