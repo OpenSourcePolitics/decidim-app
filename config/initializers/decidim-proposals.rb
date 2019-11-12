45a507d2e3d33bed39f311478ee8f7fcfcd5efc5
Armand <fardeauarmand@gmail.com>
2019-11-12 12:14:59 +0100

    Add default settings for proposals comparator

---
+++ config/initializers/decidim-proposals.rb
@@ -0,0 +1,8 @@
+Decidim::Proposals.configure do |config|
+  # 0 means that 0% is required for the proposal to be proposed to the comparator.
+  # 100% means the proposals have to be fully similar.
+  config.similarity_threshold = 0.5
+
+  # Number of proposals displayed
+  config.similarity_limit = 2 # default value
+end
