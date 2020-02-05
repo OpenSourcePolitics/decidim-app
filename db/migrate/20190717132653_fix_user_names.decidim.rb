7f0213494528f820145bf4e291916868ce27215b
moustachu <moustachu@opensourcepolitics.eu>
2020-02-05 16:46:10 +0100

    Update 20190717132653_fix_user_names.decidim.rb

--- db/migrate/20190717132653_fix_user_names.decidim.rb
+++ db/migrate/20190717132653_fix_user_names.decidim.rb
@@ -17,8 +17,7 @@ class FixUserNames < ActiveRecord::Migration[5.2]
 
         entity.name = entity.name.delete(characters_to_remove).strip
         sanitized_nickname = entity.nickname.delete(characters_to_remove).strip
-        entity.nickname = Decidim::UserBaseEntity.nicknamize(sanitized_nickname, organization: entity.organization)
-        entity.save!
+        entity.update_columns(nickname: Decidim::UserBaseEntity.nicknamize(sanitized_nickname, organization: entity.organization))
       end
     end
   end
