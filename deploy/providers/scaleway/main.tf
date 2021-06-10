d1efa3f5a8f4d0f9e91e98dc0ef02bdb54aa4f6d
MoretS <s.moret@ecole-ipssi.net>
2021-06-10 17:48:26 +0200

    Remove remote-exec and local-exec provisioners

--- deploy/providers/scaleway/main.tf
+++ deploy/providers/scaleway/main.tf
@@ -44,20 +44,4 @@ resource "scaleway_instance_server" "decidim_app" {
   ip_id = scaleway_instance_ip.decidim_app_public_ip.id
 
   security_group_id = scaleway_instance_security_group.decidim_app_sg.id
-
-  provisioner "remote-exec" {
-  inline = ["sudo apt update -y", "sudo apt install python3 -y", "echo Done!"]
-
-  connection {
-    host        = self.public_ip
-    type        = "ssh"
-    user        = var.user
-    private_key = file(var.pvt_key)
-    port        = var.sg_ssh_port
-  }
-}
-
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ${self.public_ip}, -u ${var.user} -e 'ansible_port=${var.sg_ssh_port}' --private-key ${var.pvt_key} ../../ansible/playbooks/deploy_osp_app.yml"
-  }
 }
