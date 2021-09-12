48a6f9a57e2c31b62d3f92ecfda1494a36dd0707
MoretS <s.moret@ecole-ipssi.net>
2021-09-12 20:42:12 +0200

    Set inbound default policy on drop

--- deploy/providers/scaleway/main.tf
+++ deploy/providers/scaleway/main.tf
@@ -18,7 +18,7 @@ resource "scaleway_instance_security_group" "decidim_app_sg" {
 
   name = var.sg_name
 
-  inbound_default_policy  = "accept"
+  inbound_default_policy  = "drop"
   outbound_default_policy = "accept"
   
   enable_default_security = "false"
