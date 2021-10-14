c229b05dec6e07082f54d2ae082c4c2f26cc67ee
paulinebessoles <pauline.bessoles@opensourcepolitics.eu>
2021-10-14 13:46:59 +0200

    Update tests

--- spec/system/decidim/authentication_spec.rb
+++ spec/system/decidim/authentication_spec.rb
@@ -44,7 +44,7 @@ describe "Authentication", type: :system do
           find("*[type=submit]").click
         end
 
-        expect(page).to have_content("You have signed up successfully")
+        expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
       end
     end
 
@@ -71,7 +71,7 @@ describe "Authentication", type: :system do
           find("*[type=submit]").click
         end
 
-        expect(page).to have_content("¡Bienvenida! Te has registrado con éxito.")
+        expect(page).to have_content("Se ha enviado un mensaje con un enlace de confirmación a tu dirección de correo electrónico. Por favor, sigue el enlace para activar tu cuenta.")
         expect(last_user.locale).to eq("es")
       end
     end
@@ -562,7 +562,7 @@ describe "Authentication", type: :system do
             find("*[type=submit]").click
           end
 
-          expect(page).to have_content("You have signed up successfully")
+          expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
         end
       end
     end
