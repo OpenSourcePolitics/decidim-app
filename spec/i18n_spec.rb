3966b1ac6f2b816b6a9dcc1026218f97f4fe154c
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-11-22 12:01:40 +0100

    Remove useless logic in i18n specs

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -26,7 +26,7 @@ describe "I18n sanity" do
     it "is normalized" do
       error_message = "The following files need to be normalized:\n" \
                       "#{non_normalized_paths.map { |path| "  #{path}" }.join("\n")}\n" \
-                      "Please run `bundle exec i18n-tasks normalize --locales #{locales.presence || Decidim.available_locales}` to fix them"
+                      "Please run `bundle exec i18n-tasks normalize --locales #{locales}` to fix them"
 
       expect(non_normalized_paths).to be_empty, error_message
     end
