36671c835684f5647388cb84764df9ab30581e42
Armand Fardeau <fardeauarmand@gmail.com>
2021-11-25 00:26:18 +0100

    Fix i18n-spec

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -7,17 +7,20 @@ describe "I18n sanity" do
     ENV["ENFORCED_LOCALES"].presence || "en,fr"
   end
 
-  let(:i18n) { I18n::Tasks::BaseTask.new(locales: locales.split(",")) }
+  let(:i18n) { I18n::Tasks::BaseTask.new({ locales: locales.split(",") }, config_file: nil) }
   let(:missing_keys) { i18n.missing_keys }
   let(:unused_keys) { i18n.unused_keys }
   let(:non_normalized_paths) { i18n.non_normalized_paths }
+  let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
 
   it "does not have missing keys" do
-    expect(missing_keys).to be_empty, "#{missing_keys.inspect} are missing"
+    expect(missing_keys).to be_empty,
+                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
 
   it "does not have unused keys" do
-    expect(unused_keys).to be_empty, "#{unused_keys.inspect} are unused"
+    expect(unused_keys).to be_empty,
+                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
 
   unless ENV["SKIP_NORMALIZATION"]
@@ -28,5 +31,11 @@ describe "I18n sanity" do
 
       expect(non_normalized_paths).to be_empty, error_message
     end
+  end
+
+  it "does not have inconsistent interpolations" do
+    error_message = "#{inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations.\n" \
+                    "Run `i18n-tasks check-consistent-interpolations' to show them"
+    expect(inconsistent_interpolations).to be_empty, error_message
   end
 end
