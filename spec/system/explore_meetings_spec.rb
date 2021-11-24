70b7fd530aa4ba3f40895267b76ad21ac27061c1
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-11-24 17:05:22 +0100

    Add slow to test

--- spec/system/explore_meetings_spec.rb
+++ spec/system/explore_meetings_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe "Explore meeting directory", type: :system do
+describe "Explore meeting directory", :slow, type: :system do
   let(:directory) do
     Decidim::Meetings::DirectoryEngine.routes.url_helpers.root_path
   end
