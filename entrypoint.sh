c22ed23947e95d7e8ce2fe2b85c16c179d7eadca
MoretS <s.moret@ecole-ipssi.net>
2021-12-01 16:34:08 +0100

    Update docker-compose + entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,5 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
-# Create database and run migrations 
-bundle exec rails db:create db:migrate
-
-# Create seeds
-DECIDIM_HOST=0.0.0.0 bundle exec rails db:seed
-
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
