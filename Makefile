a86fc5c59681c41c4c87beade77e4242fbf359a7
MoretS <s.moret@ecole-ipssi.net>
2021-12-20 18:21:45 +0100

    Add kubernetes config + update assets configuration

--- Makefile
+++ Makefile
@@ -81,3 +81,20 @@ stop:
 delete:
 	@make stop
 	docker volume prune
+
+redis-setup:
+	helm install redis-cluster bitnami/redis \
+	  --set auth.enabled=false \
+      --set cluster.slaveCount=3 \
+      --set securityContext.enabled=true \
+      --set securityContext.fsGroup=2000 \
+      --set securityContext.runAsUser=1000 \
+      --set volumePermissions.enabled=true \
+      --set master.persistence.enabled=true \
+      --set master.persistence.path=/data \
+      --set master.persistence.size=8Gi \
+      --set master.persistence.storageClass="scw-bssd-retain" \
+      --set slave.persistence.enabled=true \
+      --set slave.persistence.path=/data \
+      --set slave.persistence.size=8Gi \
+      --set slave.persistence.storageClass="scw-bssd-retain"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,34 +1,37 @@
 # frozen_string_literal: true
 
-# Default CarrierWave setup.
-#
-CarrierWave.configure do |config|
-  config.permissions = 0o666
-  config.directory_permissions = 0o777
-  config.storage = :file
-  config.enable_processing = !Rails.env.test?
-  # This needs to be set for correct attachment file URLs in emails
-  # DON'T FORGET to ALSO set this in `config/application.rb`
-  # config.asset_host = "https://your.server.url"
-end
-
-# Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
-if ENV["HEROKU_APP_NAME"].present? && Rails.env.production?
-  require "carrierwave/storage/fog"
-
+if Rails.application.secrets.dig(:scaleway, :id).present?
   CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_provider = "fog/aws" # required
+    config.fog_provider = "fog/aws"
     config.fog_credentials = {
-      provider: "AWS", # required
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region: "eu-central-1", # optional, defaults to 'us-east-1'
-      host: "s3.eu-central-1.amazonaws.com" # optional, defaults to nil
+      provider: "AWS",
+      aws_access_key_id: Rails.application.secrets.dig(:scaleway, :id),
+      aws_secret_access_key: Rails.application.secrets.dig(:scaleway, :token),
+      aws_signature_version: 4,
+      region: "fr-par",
+      host: "s3.fr-par.scw.cloud",
+      endpoint: "https://s3.fr-par.scw.cloud",
+      enable_signature_v4_streaming: false
     }
-    config.fog_directory = "decidim-heroku" # required
-    config.fog_public = true # optional, defaults to true
-    config.fog_attributes = { "Cache-Control" => "max-age=#{365.days.to_i}" } # optional, defaults to {}
     config.storage = :fog
+    # config.fog_use_ssl_for_aws = false
+    # config.enable_processing = false
+    # config.fog_public = false # optional, defaults to true
+    config.fog_directory = Rails.application.secrets.dig(:scaleway, :bucket_name)
+    config.fog_attributes = {
+      "Cache-Control" => "max-age=#{365.days.to_i}",
+      "X-Content-Type-Options" => "nosniff"
+    }
+    config.asset_host = "https://#{Rails.application.secrets[:asset_host]}" if Rails.application.secrets[:asset_host].present?
+  end
+else
+  CarrierWave.configure do |config|
+    config.permissions = 0o666
+    config.directory_permissions = 0o777
+    config.storage = :file
+    config.enable_processing = !Rails.env.test?
+    # This needs to be set for correct attachment file URLs in emails
+    # DON'T FORGET to ALSO set this in `config/application.rb`
+    # config.asset_host = "https://your.server.url"
   end
 end
