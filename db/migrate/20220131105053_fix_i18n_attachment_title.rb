85485c9844d236a0097cc7ba9b82636533f1006b
moustachu <moustachu@opensourcepolitics.eu>
2022-01-31 11:54:21 +0100

    Fix i18n attachment titles

---
+++ db/migrate/20220131105053_fix_i18n_attachment_title.rb
@@ -0,0 +1,22 @@
+class FixI18nAttachmentTitle < ActiveRecord::Migration[5.2]
+  def change
+    PaperTrail.request(enabled: false) do
+      Decidim::Attachment.find_each do |attachment|
+        fix_content(attachment, :title)
+        fix_content(attachment, :description)
+
+        attachment.save! if attachment.has_changes_to_save?
+      end
+    end
+  end
+
+  def fix_content(resource, attribute)
+    if resource.send(attribute).is_a?(Hash)
+      resource.send(attribute).keys.each do |k|
+        if resource.send(attribute)[k].is_a?(Hash) && resource.send(attribute)[k].has_key?(k) && resource.send(attribute)[k][k].is_a?(String)
+          resource.send(attribute)[k] = resource.send(attribute)[k][k]
+        end
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_181438) do
+ActiveRecord::Schema.define(version: 2022_01_31_105053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
