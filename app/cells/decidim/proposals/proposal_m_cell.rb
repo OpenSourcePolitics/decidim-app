41a0c1921a6dd842ab419abb985acd7033956687
paulinebessoles <pauline.bessoles@opensourcepolitics.eu>
2022-03-11 12:22:00 +0100

    Lint code

--- app/cells/decidim/proposals/proposal_m_cell.rb
+++ app/cells/decidim/proposals/proposal_m_cell.rb
@@ -126,26 +126,23 @@ module Decidim
       end
 
       def cache_hash
-        def perform_caching?
-          hash = []
-          false
-          hash << "decidim/proposals/proposal_m"
-          hash << I18n.locale.to_s
-          hash << model.cache_key_with_version
-          hash << model.proposal_votes_count
-          hash << model.endorsements_count
-          hash << Digest::MD5.hexdigest(model.component.cache_key_with_version)
-          hash << Digest::MD5.hexdigest(resource_image_path) if resource_image_path
-          if current_user
-            hash << current_user.cache_key_with_version
-            hash << current_user.follows?(model) ? 1 : 0
-          end
-          hash << model.follows_count
-          hash << Digest::MD5.hexdigest(model.authors.map(&:cache_key_with_version).to_s)
-          hash << context[:controller].class
-
-          hash.join("/")
+        hash = []
+        hash << "decidim/proposals/proposal_m"
+        hash << I18n.locale.to_s
+        hash << model.cache_key_with_version
+        hash << model.proposal_votes_count
+        hash << model.endorsements_count
+        hash << Digest::MD5.hexdigest(model.component.cache_key_with_version)
+        hash << Digest::MD5.hexdigest(resource_image_path) if resource_image_path
+        if current_user
+          hash << current_user.cache_key_with_version
+          hash << current_user.follows?(model) ? 1 : 0
         end
+        hash << model.follows_count
+        hash << Digest::MD5.hexdigest(model.authors.map(&:cache_key_with_version).to_s)
+        hash << context[:controller].class
+
+        hash.join("/")
       end
     end
   end
