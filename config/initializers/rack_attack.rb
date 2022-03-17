2434e9da4d0dc7ddae5f4b36c79e405817735f13
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-03-17 15:48:34 +0100

    Fix Rack Attack throttle only for production mode

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-class Rack::Attack
-  throttle("req/ip", limit: 100, period: 1.minute) do |req|
-    Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
-    req.ip unless req.path.start_with?("/assets")
+if Rails.env.production?
+  class Rack::Attack
+    throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
+      Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
+      req.ip unless req.path.start_with?("/assets")
+    end
   end
 end
+
