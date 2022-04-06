3f21e0d74068e616116e0483a772d4830872a55d
eliegaboriau <elie@opensourcepolitics.eu>
2022-04-06 14:59:50 +0200

    add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_142155) do
+ActiveRecord::Schema.define(version: 2022_04_06_125821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -287,7 +287,7 @@ ActiveRecord::Schema.define(version: 2022_04_05_142155) do
   end
 
   create_table "decidim_awesome_config", force: :cascade do |t|
-    t.string "var"
+    t.jsonb "var"
     t.jsonb "value"
     t.integer "decidim_organization_id"
     t.datetime "created_at", null: false
