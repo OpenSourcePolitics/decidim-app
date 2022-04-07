5aeeccc8cd783f28cf3c7ad7d564e574746d0d09
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-07 18:50:44 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_135855) do
+ActiveRecord::Schema.define(version: 2022_02_21_135309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -265,7 +265,7 @@ ActiveRecord::Schema.define(version: 2022_03_21_135855) do
   end
 
   create_table "decidim_awesome_config", force: :cascade do |t|
-    t.string "var"
+    t.jsonb "var"
     t.jsonb "value"
     t.integer "decidim_organization_id"
     t.datetime "created_at", null: false
