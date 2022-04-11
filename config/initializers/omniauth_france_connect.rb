810425a2d3ebe03dd0cc18143ede2464e99530d0
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-11 11:04:44 +0200

    Add FC profile and FC UID

--- config/initializers/omniauth_france_connect.rb
+++ config/initializers/omniauth_france_connect.rb
@@ -12,7 +12,35 @@ if Rails.application.secrets.dig(:omniauth, :france_connect).present?
         env["omniauth.strategy"].options[:client_secret] = provider_config[:client_secret]
         env["omniauth.strategy"].options[:site] = provider_config[:site_url]
       },
-      scope: "openid email"
+      scope: [:email, :openid, :birthdate, :given_name, :family_name]
+    )
+  end
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider(
+      :france_connect_profile,
+      setup: lambda { |env|
+        request = Rack::Request.new(env)
+        organization = Decidim::Organization.find_by(host: request.host)
+        provider_config = organization.enabled_omniauth_providers[:france_connect_profile]
+        env["omniauth.strategy"].options[:client_id] = provider_config[:client_id]
+        env["omniauth.strategy"].options[:client_secret] = provider_config[:client_secret]
+        env["omniauth.strategy"].options[:site] = provider_config[:site_url]
+      },
+      scope: [:email, :openid, :birthdate, :given_name, :family_name, :preferred_username]
+    )
+  end
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider(
+      :france_connect_uid,
+      setup: lambda { |env|
+        request = Rack::Request.new(env)
+        organization = Decidim::Organization.find_by(host: request.host)
+        provider_config = organization.enabled_omniauth_providers[:france_connect_uid]
+        env["omniauth.strategy"].options[:client_id] = provider_config[:client_id]
+        env["omniauth.strategy"].options[:client_secret] = provider_config[:client_secret]
+        env["omniauth.strategy"].options[:site] = provider_config[:site_url]
+      },
+      scope: [:openid]
     )
   end
 end
