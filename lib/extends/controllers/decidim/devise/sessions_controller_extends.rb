0f8422fb55e0a36b21b4cedca9b7abb179c1bd1f
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-15 14:26:14 +0200

    Extends Session controller and skip AH

---
+++ lib/extends/controllers/decidim/devise/sessions_controller_extends.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module SessionControllerExtends
+  def after_sign_in_path_for(user)
+    if user.present? && user.blocked?
+      check_user_block_status(user)
+    elsif first_login_and_not_authorized?(user) && !user.admin? && !pending_redirect?(user) && !skip_authorization_handler?
+      decidim_verifications.first_login_authorizations_path
+    else
+      super
+    end
+  end
+
+  private
+
+  # Skip authorization handler by default
+  def skip_authorization_handler?
+    ENV["SKIP_FIRST_LOGIN_AUTHORIZATION"] ? ActiveRecord::Type::Boolean.new.cast(ENV["SKIP_FIRST_LOGIN_AUTHORIZATION"]) : true
+  end
+end
+
+Decidim::Devise::SessionsController.class_eval do
+  prepend(SessionControllerExtends)
+end
