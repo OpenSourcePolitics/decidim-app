3921729a030b572b5b5201bc0c9b1e8c0fe9589a
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-04-28 15:04:39 +0200

    Remove unused files

--- db/migrate/20220406125819_add_geo_json_to_scopes.decidim_homepage_interactive_map.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# This migration comes from decidim_homepage_interactive_map (originally 20191125143402)
-
-class AddGeoJsonToScopes < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_scopes, :geojson, :jsonb
-  end
-end
--- db/migrate/20220406125821_add_display_linked_assemblies_to_participatory_processes.decidim_homepage_interactive_map.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# This migration comes from decidim_homepage_interactive_map (originally 20201113100047)
-
-class AddDisplayLinkedAssembliesToParticipatoryProcesses < ActiveRecord::Migration[5.2]
-  def change
-    add_column :decidim_participatory_processes, :display_linked_assemblies, :boolean, default: false
-  end
-end
