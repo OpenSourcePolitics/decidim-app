9b677c79eff3dff82af418e25cf0186266a8845c
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2022-06-20 18:12:30 +0200

    [Backport] - PR 8734 'Fix order by weight in processes groups' (#133)

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require "extends/controllers/decidim/devise/sessions_controller_extends"
+require "extends/queries/decidim/participatory_processes/group_participatory_processes_extends"
