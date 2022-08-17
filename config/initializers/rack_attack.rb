ffe7cde8137bdae46fa5c6836a7119f1620bb0ad
armandfardeau <fardeauarmand@gmail.com>
2022-08-17 14:54:07 +0200

    Move logging to suscriber

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,9 +2,12 @@
 
 if Rails.env.production?
   Rack::Attack.throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
-    unless req.path.start_with?("/assets") || req.path.start_with?("/rails/active_storage")
-      Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
-      req.ip
-    end
+    req.ip unless req.path.start_with?("/assets") || req.path.start_with?("/rails/active_storage")
+  end
+
+  ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |_name, _start, _finish, _request_id, payload|
+    req = payload[:request]
+
+    Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
   end
 end
