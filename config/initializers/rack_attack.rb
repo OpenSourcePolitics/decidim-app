3b7973cdd531d097274bd3bb5612404b8130b370
armandfardeau <fardeauarmand@gmail.com>
2022-08-17 10:34:30 +0200

    Fix rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  class Rack::Attack
-    throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
+  Rack::Attack.throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
+    unless req.path.start_with?("/assets") || req.path.start_with?("/rails/active_storage")
       Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
-      req.ip unless req.path.start_with?("/assets")
+      req.ip
     end
   end
 end
