0973b35b2b24cf48055c6ef26c45c9b1ceee4c71
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-13 16:12:14 +0200

    Throttle 10requests / second

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,7 +2,8 @@
 
 if Rails.env.production?
   class Rack::Attack
-    throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
+    # source: https://github.com/rack/rack-attack/blob/4d201f7e425f99a0c1f0956fbcc935614d695308/examples/rack_attack.rb#L5
+    throttle("req/ip", limit: 10, period: 1) do |req|
       Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
       req.ip unless req.path.start_with?("/assets")
     end
