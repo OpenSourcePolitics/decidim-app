d90420f0028ad7a6a218212276661cfcb190584d
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-19 15:01:07 +0200

    Rack Attack - Throttle 100 requests / minute (#157)
    * Throttle 10requests / second
    
    * Update rack attack rules

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  class Rack::Attack
-    throttle("req/ip", limit: Decidim.throttling_max_requests, period: Decidim.throttling_max_requests) do |req|
-      Rails.logger.warn("[Rack::Attack] [THROTTLE - req / ip] :: #{req.ip} :: #{req.path} :: #{req.GET}")
-      req.ip unless req.path.start_with?("/assets")
-    end
+  Rack::Attack.throttle("req/ip",
+                        limit: Decidim.throttling_max_requests,
+                        period: Decidim.throttling_period) do |req|
+    next if req.path.start_with?("/assets")
+    next if req.path.start_with?("/rails/active_storage")
+
+    rack_logger = Rails.root.join("log/rack_attack.log")
+    rack_logger.warn("[Rack::Attack] [THROTTLE - req / ip] | #{req.ip} | #{req.path} | #{req.GET}")
+
+    req.ip
   end
 end
