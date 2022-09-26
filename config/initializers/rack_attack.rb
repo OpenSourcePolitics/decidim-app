68be3784f1d59faab3ad5ffd9d892ea5b4832561
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-26 18:16:38 +0200

    Update Rack Attack configuration

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,14 +1,31 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  Rack::Attack.throttle("req/ip",
-                        limit: Decidim.throttling_max_requests,
-                        period: Decidim.throttling_period) do |req|
-    next if req.path.start_with?("/assets")
-    next if req.path.start_with?("/rails/active_storage")
 
+Rack::Attack.enabled = ENV['ENABLE_RACK_ATTACK'] || Rails.env.production?
+if !ENV['MEMCACHEDCLOUD_SERVERS'] || Rails.env.test?
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+end
+
+
+Rack::Attack.throttle("req/ip",
+                      limit: Decidim.throttling_max_requests,
+                      period: Decidim.throttling_period) do |req|
+
+  unless req.path.start_with?("/assets") || req.path.start_with?("/rails/active_storage")
     rack_logger = Logger.new(Rails.root.join("log/rack_attack.log"))
-    rack_logger.warn("[Rack::Attack] [THROTTLE - req / ip] | #{req.ip} | #{req.path} | #{req.GET}")
+
+    request_uuid = req.env['action_dispatch.request_id']
+
+    params = {
+      "ip" => req.ip,
+      "path" => req.path,
+      "get" => req.GET,
+      "post" => req.POST,
+      "host" => req.host,
+      "referer" => req.referer
+    }
+
+    rack_logger.warn("[#{request_uuid}] #{params}")
 
     req.ip
   end
