d5ced6cfd9a1171f4950c37f80c412df8c8e4412
moustachu <moustachu@opensourcepolitics.eu>
2022-09-26 22:48:09 +0200

    Update rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
-  
   # Remove the original throttle fron decidim-core
   # see https://github.com/decidim/decidim/blob/release/0.26-stable/decidim-core/config/initializers/rack_attack.rb#L19
   Rails.application.config.after_initialize do
     Rack::Attack.throttles.delete("requests by ip")
   end
-  
   Rack::Attack.throttle("req/ip",
                         limit: Decidim.throttling_max_requests,
                         period: Decidim.throttling_period) do |req|
