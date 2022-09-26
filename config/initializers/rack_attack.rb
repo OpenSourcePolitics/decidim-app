71ea083e1d7b543614b35f5e31826e28b5b8d7bf
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-26 21:29:29 +0200

    Configure HTMl template for 429 error

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,10 +4,41 @@
 # Can be deactivated with 'ENABLE_RACK_ATTACK="2"'
 Rack::Attack.enabled = (ENV['ENABLE_RACK_ATTACK'] == "1") || Rails.env.production?
 
+Rack::Attack.throttled_response_retry_after_header = true
+
 # By default use the memory store for inspecting requests
 # Better to use MemCached or Redis in production mode
 if !ENV['MEMCACHEDCLOUD_SERVERS'] || Rails.env.test?
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+end
+
+Rack::Attack.throttled_responder = lambda do |request|
+  match_data = request.env['rack.attack.match_data']
+  now = match_data[:epoch_time]
+  # throttle_time = match_data[:period]
+  until_period = I18n.l(Time.at(now + 60).to_datetime, format: :decidim_short)
+
+  # headers = {
+  #   'RateLimit-Limit' => match_data[:limit].to_s,
+  #   'RateLimit-Remaining' => '0',
+  #   'RateLimit-Reset' => (now + (match_data[:period] - now % match_data[:period])).to_s
+  # }
+
+  rack_logger = Logger.new(Rails.root.join("log/rack_attack.log"))
+
+  request_uuid = request.env['action_dispatch.request_id']
+  params = {
+    "ip" => request.ip,
+    "path" => request.path,
+    "get" => request.GET,
+    "post" => request.POST,
+    "host" => request.host,
+    "referer" => request.referer
+  }
+
+  rack_logger.warn("[#{request_uuid}] #{params}")
+
+  [ 429, {'Content-Type' => 'text/html'}, [html_template(until_period)]]
 end
 
 Rack::Attack.throttle("req/ip",
@@ -15,21 +46,81 @@ Rack::Attack.throttle("req/ip",
                       period: Decidim.throttling_period) do |req|
 
   unless req.path.start_with?("/assets") || req.path.start_with?("/rails/active_storage")
-    unless Rails.env.test?
-      rack_logger = Logger.new(Rails.root.join("log/rack_attack.log"))
-
-      request_uuid = req.env['action_dispatch.request_id']
-      params = {
-        "ip" => req.ip,
-        "path" => req.path,
-        "get" => req.GET,
-        "post" => req.POST,
-        "host" => req.host,
-        "referer" => req.referer
-      }
-
-      rack_logger.warn("[#{request_uuid}] #{params}")
-    end
     req.ip
   end
+end
+
+
+def html_template(until_period)
+  "
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Too many requests</title>
+  <meta name='viewport' content='width=device-width,initial-scale=1'>
+  <style>
+  .rails-default-error-page {
+    background-color: #EFEFEF;
+    color: #2E2F30;
+    text-align: center;
+    font-family: arial, sans-serif;
+    margin: 0;
+  }
+
+  .rails-default-error-page div.dialog {
+    width: 95%;
+    max-width: 33em;
+    margin: 4em auto 0;
+  }
+
+  .rails-default-error-page div.dialog > div {
+    border: 1px solid #CCC;
+    border-right-color: #999;
+    border-left-color: #999;
+    border-bottom-color: #BBB;
+    border-top: #B00100 solid 4px;
+    border-top-left-radius: 9px;
+    border-top-right-radius: 9px;
+    background-color: white;
+    padding: 7px 12% 0;
+    box-shadow: 0 3px 8px rgba(50, 50, 50, 0.17);
+  }
+
+  .rails-default-error-page h1 {
+    font-size: 100%;
+    color: #730E15;
+    line-height: 1.5em;
+  }
+
+  .rails-default-error-page div.dialog > p {
+    margin: 0 0 1em;
+    padding: 1em;
+    background-color: #F7F7F7;
+    border: 1px solid #CCC;
+    border-right-color: #999;
+    border-left-color: #999;
+    border-bottom-color: #999;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-top-color: #DADADA;
+    color: #666;
+    box-shadow: 0 3px 8px rgba(50, 50, 50, 0.17);
+  }
+  </style>
+</head>
+
+<body class='rails-default-error-page'>
+  <!-- This file lives in public/422.html -->
+  <div class='dialog'>
+    <div>
+      <h1>429 - Too many requests</h1>
+      <p>#{I18n.t('rack_attack.too_many_requests.message')}</p>
+
+      <b>#{I18n.t('rack_attack.too_many_requests.time', period: until_period)}</b>
+    </div>
+  </div>
+</body>
+</html>
+
+"
 end
--- spec/lib/rack_attack_spec.rb
+++ spec/lib/rack_attack_spec.rb
@@ -14,8 +14,9 @@ describe Rack::Attack, type: :request do
     Rack::Attack.enabled = false
   end
 
-  describe "GET decidim.root_pqth" do
+  describe "GET decidim.root_path" do
     let(:headers) { { "REMOTE_ADDR" => "1.2.3.4", "decidim.current_organization" => organization } }
+
     it "successful for 100 requests, then blocks the user nicely" do
       100.times do
         get decidim.root_path, params: {}, headers: headers
@@ -23,10 +24,26 @@ describe Rack::Attack, type: :request do
       end
 
       get decidim.root_path, params: {}, headers: headers
-      expect(response.body).to include("Retry later")
+      expect(response.body).to include("Your connection have been slowed because server received too many requests.")
       expect(response).to have_http_status(:too_many_requests)
 
-      travel_to(10.minutes.from_now) do
+      travel_to(1.minutes.from_now) do
+        get decidim.root_path, params: {}, headers: headers
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    it "successful for 99 requests" do
+      99.times do
+        get decidim.root_path, params: {}, headers: headers
+        expect(response).to have_http_status(:ok)
+      end
+
+      get decidim.root_path, params: {}, headers: headers
+      expect(response.body).not_to include("Your connection have been slowed because server received too many requests.")
+      expect(response).not_to have_http_status(:too_many_requests)
+
+      travel_to(1.minutes.from_now) do
         get decidim.root_path, params: {}, headers: headers
         expect(response).to have_http_status(:ok)
       end
