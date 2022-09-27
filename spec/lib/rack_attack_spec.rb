fec3610f0754ce12e1829a4866dccc780803a6e8
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-27 15:23:16 +0200

    Update rack attack specs

--- spec/lib/rack_attack_spec.rb
+++ spec/lib/rack_attack_spec.rb
@@ -18,15 +18,15 @@ describe "Rack::Attack", type: :request do
   describe "Throttling" do
     let(:headers) { { "REMOTE_ADDR" => "1.2.3.4", "decidim.current_organization" => organization } }
 
-    it "successful for 100 requests, then blocks the user nicely" do
+    it "successful for 100 requests, then blocks the user" do
       100.times do
         get decidim.root_path, params: {}, headers: headers
         expect(response).to have_http_status(:ok)
       end
 
       get decidim.root_path, params: {}, headers: headers
-      expect(response.body).to include("Your connection have been slowed because server received too many requests.")
       expect(response).to have_http_status(:too_many_requests)
+      expect(response.body).to include("Your connection have been slowed because server received too many requests.")
 
       travel_to(1.minute.from_now) do
         get decidim.root_path, params: {}, headers: headers
@@ -53,9 +53,10 @@ describe "Rack::Attack", type: :request do
 
   describe "Fail2Ban" do
     let(:headers) { { "REMOTE_ADDR" => "1.2.3.4", "decidim.current_organization" => organization } }
+    let(:invalid_routes) { ['/etc/passwd', '/wp-admin/', '/wp-login/', 'SELECT', 'CONCAT', 'UNION SELECT', '/.git/'] }
 
-    it "Block directly requests for invalid routes for 1 hour" do
-      get "#{decidim.root_path}/etc/passwd", params: {}, headers: headers
+    shared_examples_for "block user for specific request" do |decidim, path|
+      get "#{decidim.root_path}#{path}", params: {}, headers: headers
       expect(response).to have_http_status(:forbidden)
 
       get decidim.root_path, params: {}, headers: headers
@@ -64,6 +65,21 @@ describe "Rack::Attack", type: :request do
       travel_to(61.minutes.from_now) do
         get decidim.root_path, params: {}, headers: headers
         expect(response).to have_http_status(:ok)
+      end
+    end
+
+    %w[/etc/passwd /wp-admin/index.php /wp-login/index.php SELECT CONCAT /.git/config].each do |path|
+      it "blocks user for specific request : '#{path}'" do
+        get "#{decidim.root_path}#{path}", params: {}, headers: headers
+        expect(response).to have_http_status(:forbidden)
+
+        get decidim.root_path, params: {}, headers: headers
+        expect(response).to have_http_status(:forbidden)
+
+        travel_to(61.minutes.from_now) do
+          get decidim.root_path, params: {}, headers: headers
+          expect(response).to have_http_status(:ok)
+        end
       end
     end
   end
