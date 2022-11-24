335dd043f7af27bfa53f0024b9d67861e4d10c84
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-11-24 13:50:44 +0100

    When logging out, if we are logged in using FC, we disconnects from FC then it invalidates our session, following that it's disconnecting us from both platforms

---
+++ app/controllers/decidim/devise/sessions_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Devise
+    # Custom Devise SessionsController to avoid namespace problems.
+    class SessionsController < ::Devise::SessionsController
+      include Decidim::DeviseControllers
+
+      # rubocop: disable Rails/LexicallyScopedActionFilter
+      before_action :check_sign_in_enabled, only: :create
+      # rubocop: enable Rails/LexicallyScopedActionFilter
+
+      def destroy
+        current_user.invalidate_all_sessions!
+        if current_organization.enabled_omniauth_providers.keys.any? { |provider| provider.match?("france") } &&
+           current_user.identities.find_by(provider: "france_connect").present?
+          redirect_to session["omniauth_logout"]
+          flash[:notice] = t("devise.sessions.signed_out")
+        elsif params[:translation_suffix].present?
+          super { set_flash_message! :notice, params[:translation_suffix], { scope: "decidim.devise.sessions" } }
+        else
+          super
+        end
+      end
+
+      def after_sign_in_path_for(user)
+        if user.present? && user.blocked?
+          check_user_block_status(user)
+        elsif first_login_and_not_authorized?(user) && !user.admin? && !pending_redirect?(user)
+          decidim_verifications.first_login_authorizations_path
+        else
+          super
+        end
+      end
+
+      # Calling the `stored_location_for` method removes the key, so in order
+      # to check if there's any pending redirect after login I need to call
+      # this method and use the value to set a pending redirect. This is the
+      # only way to do this without checking the session directly.
+      def pending_redirect?(user)
+        store_location_for(user, stored_location_for(user))
+      end
+
+      def first_login_and_not_authorized?(user)
+        user.is_a?(User) && user.sign_in_count == 1 && current_organization.available_authorizations.any? && user.verifiable?
+      end
+
+      def after_sign_out_path_for(user)
+        request.referer || super
+      end
+
+      private
+
+      def check_sign_in_enabled
+        redirect_to new_user_session_path unless current_organization.sign_in_enabled?
+      end
+    end
+  end
+end
