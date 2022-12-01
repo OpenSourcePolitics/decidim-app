ada60a3de2511594dac41aa086d063a0fa6f5273
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-12-01 16:41:06 +0100

    Fetch organization directly from memory

--- config/initializers/omniauth_france_connect.rb
+++ config/initializers/omniauth_france_connect.rb
@@ -6,7 +6,7 @@ if Rails.application.secrets.dig(:omniauth, :france_connect).present?
       :france_connect,
       setup: lambda { |env|
         request = Rack::Request.new(env)
-        organization = Decidim::Organization.find_by(host: request.host)
+        organization = env["decidim.current_organization"].presence || Decidim::Organization.find_by(host: request.host)
         provider_config = organization.enabled_omniauth_providers[:france_connect]
         env["omniauth.strategy"].options[:client_id] = provider_config[:client_id]
         env["omniauth.strategy"].options[:client_secret] = provider_config[:client_secret]
