a68eec7b1fd811ec15ddf9d82a7da8fa8ce54f16
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-01-20 14:30:57 +0100

    fix when error in non string field

---
+++ app/helpers/decidim/forms/application_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Forms
+    # Custom helpers, scoped to the forms engine.
+    module ApplicationHelper
+      # Show cell for selected models
+      def show_represent_user_group?
+        model_name = questionnaire_for.model_name.element
+
+        permited_models.include?(model_name)
+      end
+      alias show_public_participation? show_represent_user_group?
+
+      def permited_models
+        %(meeting)
+      end
+
+      def invalid?(responses)
+        bool = false
+        responses.each do |response|
+          bool ||= response.errors.any?
+        end
+        bool
+      end
+    end
+  end
+end
