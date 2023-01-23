54b8e285954032e13ade18b0ee44c2dfcd748f66
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-01-23 14:31:42 +0100

    Add cache_hash on project list item cell (#227)

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -2,3 +2,4 @@
 
 require "extends/controllers/decidim/devise/account_controller_extends"
 require "extends/controllers/decidim/meetings/meetings_controller_extends"
+require "extends/cells/decidim/budgets/project_list_item_cell_extends"
