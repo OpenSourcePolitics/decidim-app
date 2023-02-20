6ed4d8675120a5dd02cfbe6fa777f46e7519d866
armandfardeau <fardeauarmand@gmail.com>
2023-02-20 11:43:41 +0100

    Use short pattern

--- lib/decidim/assets_hash.rb
+++ lib/decidim/assets_hash.rb
@@ -5,7 +5,7 @@ require "digest"
 module Decidim
   class AssetsHash
     def initialize
-      @assets_patterns = %w(Gemfile Gemfile.lock package.json yarn.lock app/assets/**/* app/packs/**/* vendor/**/* packages/**/* lib/assets/**/*)
+      @assets_patterns = %w(Gemfile* package* yarn* app/assets/**/* app/packs/**/* vendor/**/* packages/**/* lib/assets/**/*)
       @included_extensions = %w(lock Gemfile gemspec json js mjs jsx ts tsx gql graphql bmp gif jpeg jpg png tiff ico avif webp eot otf ttf woff woff2 svg md odt)
     end
 
