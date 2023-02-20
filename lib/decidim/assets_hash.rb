25074a23e29bde9210cdb8bf362523502cbcd03a
armandfardeau <fardeauarmand@gmail.com>
2023-02-20 11:23:49 +0100

    Add gemspec to allowlist

--- lib/decidim/assets_hash.rb
+++ lib/decidim/assets_hash.rb
@@ -6,7 +6,7 @@ module Decidim
   class AssetsHash
     def initialize
       @assets_patterns = %w(Gemfile Gemfile.lock package.json yarn.lock app/assets/**/* app/packs/**/* vendor/**/* packages/**/* lib/assets/**/*)
-      @included_extensions = %w(lock Gemfile json js mjs jsx ts tsx gql graphql bmp gif jpeg jpg png tiff ico avif webp eot otf ttf woff woff2 svg md odt)
+      @included_extensions = %w(lock Gemfile gemspec json js mjs jsx ts tsx gql graphql bmp gif jpeg jpg png tiff ico avif webp eot otf ttf woff woff2 svg md odt)
     end
 
     def self.run(output: true)
