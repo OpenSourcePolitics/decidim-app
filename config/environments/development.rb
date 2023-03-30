11695509797d39f4fbd5d0086d86c225ccd6ed44
quentinchampenois <quentin.champenois7@gmail.com>
2023-03-30 14:49:08 +0200

    Comment sidekiq for dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.active_job.queue_adapter = :sidekiq
+  # config.active_job.queue_adapter = :sidekiq
 end
