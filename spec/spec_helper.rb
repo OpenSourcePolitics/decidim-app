d79e4f6132551c385208b268dfb8edc386847f14
armandfardeau <fardeauarmand@gmail.com>
2023-04-26 17:15:21 +0200

    use documentation format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require "decidim/dev/test/base_spec_helper"
 Dir.glob("./spec/support/**/*.rb").sort.each { |f| require f }
 
 RSpec.configure do |config|
+  config.formatter = ENV.fetch("RSPEC_FORMAT", "progress").to_sym
   config.include EnvironmentVariablesHelper
 
   config.before do
