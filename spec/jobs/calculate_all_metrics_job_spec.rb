714b83b3868abf0765e3e6d6ab89c823a073a660
quentinchampenois <quentin.champenois7@gmail.com>
2023-05-11 18:08:39 +0200

    Update job specs

--- spec/jobs/calculate_all_metrics_job_spec.rb
+++ spec/jobs/calculate_all_metrics_job_spec.rb
@@ -2,12 +2,27 @@
 
 require "spec_helper"
 
-describe CalculateAllMetricsJob do
-  subject { described_class }
+describe CalculateAllMetricsJob, type: :job do
+  let(:organization) { create(:organization) }
+  let(:metric_manifest) { Decidim::MetricManifest.new(metric_name: "metric") }
 
-  let(:task) { Rake::Task["decidim:metrics:all"] }
+  describe '#perform' do
+    before do
+      allow(Decidim::Organization).to receive(:find_each).and_yield(organization)
+      allow(Decidim.metrics_registry).to receive(:all).and_return([metric_manifest])
+    end
 
-  it "doesn't raise an error" do
-    expect { subject.perform_now }.not_to raise_error
+    it 'calls MetricJob for each organization and metric_manifest' do
+      expect(Decidim::MetricJob).to receive(:perform_later).with(metric_manifest.manager_class, organization, nil)
+      subject.perform
+    end
+  end
+
+  describe '#call_metric_job' do
+    it 'enqueues the MetricJob with the correct arguments' do
+      expect(Decidim::MetricJob).to receive(:perform_later).with(metric_manifest.manager_class, organization, nil)
+
+      subject.send(:call_metric_job, metric_manifest, organization)
+    end
   end
 end
