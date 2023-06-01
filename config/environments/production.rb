72fb71c4981d7f3e4a52e2cb9d1d6156bde1e54e
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-06-01 11:22:55 +0200

    Add machine translation (#304)
    Co-authored-by: Pauline Bessoles <52420208+paulinebessoles@users.noreply.github.com>
    Co-authored-by: paulinebessoles <pauline.bessoles@opensourcepolitics.eu>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter = :sidekiq
-  # see confguration for sidekiq in `config/sidekiq.yml`
+  # see configuration for sidekiq in `config/sidekiq.yml`
   # config.active_job.queue_name_prefix = "development_app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
