f9527d7ac0a3349f06a8903d865c4267a1b4e8db
Pierre Ozoux <pierre@ozoux.net>
2023-07-04 10:04:41 +0200

    feat: improve k8s rake (#338)
    Co-authored-by: armandfardeau <fardeauarmand@gmail.com>

--- lib/k8s_configuration_exporter.rb
+++ lib/k8s_configuration_exporter.rb
@@ -6,15 +6,27 @@ require "k8s_organization_exporter"
 class K8sConfigurationExporter
   EXPORT_PATH = Rails.root.join("tmp/k8s-migration")
 
-  def initialize(image, enable_sync)
+  def initialize(image = "")
     @image = image
-    @enable_sync = enable_sync
     @organizations = Decidim::Organization.all
-    @logger = LoggerWithStdout.new("log/#{hostname}-k8s-export-#{Time.zone.now.strftime("%Y-%m-%d-%H-%M-%S")}.log")
+    @logger = LoggerWithStdout.new("log/k8s-export-#{Time.zone.now.strftime("%Y-%m-%d-%H-%M-%S")}.log")
   end
 
-  def self.export!(image, enable_sync)
-    new(image, enable_sync).export!
+  def self.dump_db
+    new.dump_db
+  end
+
+  def dump_db
+    @logger.info("found #{@organizations.count} organization#{"s" if @organizations.count.positive?}")
+    @logger.info("-------------------------")
+    @organizations.find_each do |organization|
+      @logger.info("Dumping database organization with host #{organization.host}")
+      K8sOrganizationExporter.dumping_database(organization, @logger, EXPORT_PATH, organization.host)
+    end
+  end
+
+  def self.export!(image)
+    new(image).export!
   end
 
   def export!
@@ -24,10 +36,8 @@ class K8sConfigurationExporter
     @logger.info("-------------------------")
     @organizations.find_each do |organization|
       @logger.info("exporting organization with host #{organization.host}")
-      K8sOrganizationExporter.export!(organization, @logger, EXPORT_PATH, hostname, @image)
+      K8sOrganizationExporter.export!(organization, @logger, EXPORT_PATH, @image)
     end
-
-    perform_sync
   end
 
   def clean_migration_directory
@@ -35,27 +45,5 @@ class K8sConfigurationExporter
     FileUtils.rm_rf(EXPORT_PATH)
     @logger.info("creating migration directory #{EXPORT_PATH}")
     FileUtils.mkdir_p(EXPORT_PATH)
-  end
-
-  def perform_sync
-    if @enable_sync
-      @logger.info("Cleaning bucket #{@hostname}-migration")
-      system("rclone delete scw-migration:#{@hostname}-migration --rmdirs --config ../scaleway.config")
-      @logger.info("Syncing export to bucket #{@hostname}-migration")
-      system("rclone copy #{EXPORT_PATH} scw-migration:#{@hostname}-migration --config ../scaleway.config --progress --copy-links")
-    else
-      @logger.info("NOT syncing export to bucket #{@hostname}-migration because ENABLE_SYNC is missing or false")
-      true
-    end
-  end
-
-  def hostname
-    # Socket.gethostname returns a string with ASCII-8BIT encoding, which is not compatible with parameterize
-    # We need to force the encoding to UTF-8 before calling parameterize on it
-    # We need to dup the string because force_encoding returns a new string and the original is frozen
-    @hostname ||= Socket.gethostname
-                        .dup
-                        .force_encoding("UTF-8")
-                        .parameterize
   end
 end
--- lib/tasks/k8s.rake
+++ lib/tasks/k8s.rake
@@ -3,13 +3,16 @@
 require "k8s_configuration_exporter"
 
 namespace :k8s do
-  desc "usage: bundle exec rails k8s:export_configuration IMAGE=<docker_image_ref> [ENABLE_SYNC=true]"
+  desc "usage: bundle exec rails k8s:dump_db"
+  task dump_db: :environment do
+    K8sConfigurationExporter.dump_db
+  end
+
+  desc "usage: bundle exec rails k8s:export_configuration IMAGE=<docker_image_ref>"
   task export_configuration: :environment do
     image = ENV["IMAGE"]
-    enable_sync = ENV["ENABLE_SYNC"] == "true"
+    raise "You must specify a docker image, usage: bundle exec rails k8s:export_configuration IMAGE=<image_ref>" if image.blank?
 
-    raise "You must specify a docker image, usage: bundle exec rails decidim:k8s:export_configuration IMAGE=<image_ref> [ENABLE_SYNC=true]" if image.blank?
-
-    K8sConfigurationExporter.export!(image, enable_sync)
+    K8sConfigurationExporter.export!(image)
   end
 end
