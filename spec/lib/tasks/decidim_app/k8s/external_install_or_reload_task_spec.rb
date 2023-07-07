6e645d52f493e77acbc0d88cd0a397fb13ae2119
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-07-07 17:42:57 +0200

    K8S - Raise error if file does not exist (#353)

--- spec/lib/tasks/decidim_app/k8s/external_install_or_reload_task_spec.rb
+++ spec/lib/tasks/decidim_app/k8s/external_install_or_reload_task_spec.rb
@@ -3,15 +3,33 @@
 require "spec_helper"
 
 describe "rake decidim_app:k8s:external_install_or_reload", type: :task do
+  let(:path) { Rails.root.join("spec/fixtures/k8s_configuration_example.yml") }
+
   it "preloads the Rails environment" do
     expect(task.prerequisites).to include "environment"
   end
 
-  it "calls db:migrate" do
-    with_modified_env path: "dummy_path" do
-      expect(DecidimApp::K8s::Manager).to receive(:run).with("dummy_path")
+  it "calls the manager service" do
+    with_modified_env path: path.to_s do
+      expect(DecidimApp::K8s::Manager).to receive(:run).with(path.to_s)
 
       task.execute
+    end
+  end
+
+  context "when path is not specified" do
+    it "raises an error" do
+      with_modified_env path: nil do
+        expect { task.execute }.to raise_error "You must specify a path to an external install configuration, path='path/to/external_install_configuration.yml'"
+      end
+    end
+  end
+
+  context "when path is specified but file does not exist" do
+    it "raises an error" do
+      with_modified_env path: "dummy_path" do
+        expect { task.execute }.to raise_error "You must specify a path to an external install configuration, path='path/to/external_install_configuration.yml'"
+      end
     end
   end
 end
