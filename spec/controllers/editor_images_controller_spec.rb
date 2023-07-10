8b6f17335985a6fbd6c7f3501590455b1a25be74
Elie Gaboriau <elie@opensourcepolitics.eu>
2023-07-10 15:59:06 +0200

    update editor images spec

--- spec/controllers/editor_images_controller_spec.rb
+++ spec/controllers/editor_images_controller_spec.rb
@@ -10,18 +10,8 @@ module Decidim
     let(:editor_images_path) { Rails.application.routes.url_helpers.editor_images_url(organization.open_data_file.blob, only_path: true) }
     let(:user) { create(:user, :confirmed, organization: organization) }
     let(:admin) { create(:user, :confirmed, :admin, organization: organization) }
-    let(:image) do
-      Rack::Test::UploadedFile.new(
-        Decidim::Dev.test_file("city.jpeg", "image/jpeg"),
-        "image/jpeg"
-      )
-    end
-    let(:invalid_image) do
-      Rack::Test::UploadedFile.new(
-        Decidim::Dev.test_file("invalid.jpeg", "image/jpeg"),
-        "image/jpeg"
-      )
-    end
+    let(:image) { upload_test_file(Decidim::Dev.test_file("city.jpeg", "image/jpeg")) }
+    let(:invalid_image) { upload_test_file(Decidim::Dev.test_file("invalid.jpeg", "image/jpeg")) }
     let(:valid_params) { { image: image } }
     let(:invalid_params) { { image: invalid_image } }
 
@@ -34,7 +24,7 @@ module Decidim
         it "doesn't create an editor image" do
           expect do
             post :create, params: valid_params
-          end.not_to(change { Decidim::EditorImage.count })
+          end.not_to(change(Decidim::EditorImage, :count))
 
           expect(response).to have_http_status(:redirect)
         end
@@ -48,7 +38,7 @@ module Decidim
         it "doesn't create an editor image" do
           expect do
             post :create, params: valid_params
-          end.not_to(change { Decidim::EditorImage.count })
+          end.not_to(change(Decidim::EditorImage, :count))
 
           expect(response).to have_http_status(:redirect)
         end
@@ -62,7 +52,7 @@ module Decidim
         it "creates an editor image" do
           expect do
             post :create, params: valid_params
-          end.to change { Decidim::EditorImage.count }.by(1)
+          end.to change(Decidim::EditorImage, :count).by(1)
 
           expect(response).to have_http_status(:ok)
         end
@@ -70,17 +60,18 @@ module Decidim
         it "returns full image url" do
           expect do
             post :create, params: valid_params
-          end.to change { Decidim::EditorImage.count }.by(1)
+          end.to change(Decidim::EditorImage, :count).by(1)
 
-          active_storage_path = Decidim::EditorImage.first.attached_uploader(:file).path
-          expect(response.body).to eq({ url: "http://#{organization.host}#{active_storage_path}", message: "Image uploaded successfully" }.to_json)
+          active_storage_path = Decidim::EditorImage.first.attached_uploader(:file).url(host: organization.host)
+          expect(response.body).to eq({ url: active_storage_path, message: "Image uploaded successfully" }.to_json)
         end
 
         context "when file is not valid" do
           it "doesn't create an editor image and returns an error message" do
+            byebug
             expect do
               post :create, params: invalid_params
-            end.not_to(change { Decidim::EditorImage.count })
+            end.not_to(change(Decidim::EditorImage, :count))
 
             expect(response).to have_http_status(:unprocessable_entity)
             expect(response.body).to include("Error uploading image")
@@ -95,10 +86,10 @@ module Decidim
           it "returns full image url" do
             expect do
               post :create, params: valid_params
-            end.to change { Decidim::EditorImage.count }.by(1)
+            end.to change(Decidim::EditorImage, :count).by(1)
 
             active_storage_path = Decidim::EditorImage.first.attached_uploader(:file).path
-            expect(response.body).to eq({ url: "http://test.host#{active_storage_path}", message: "Image uploaded successfully" }.to_json)
+            expect(response.body).to eq({ url: "http://test.host:3000#{active_storage_path}", message: "Image uploaded successfully" }.to_json)
           end
         end
       end
