19b0fc261a2b148d09f6439fe5daacad7503d3a4
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-08-02 16:56:11 +0200

    Ref: Fix url in content (#385)
    * ref: Add documentation for RepairUrlInContentService class
    
    * fix: Add specs for repair url in content service
    
    * ref: Use in? rather than include? on columns model
    
    * Add specs
    
    * ref: Refactor repair url service and remove schema method
    
    * fix: Add specs for #records_for
    
    * feat: Reimplement homepage interactive map
    
    * fix: Test when model can't be constantized
    
    * fix: Ensure a unique SQL query is created
    
    * ref: Rename @endpoint var to @deprecated_endpoint
    
    * fix: Add LoggerWithStdout logger in repair_url_in_content_service
    
    * fix: Add DEFAULT_LOGGER for service
    
    * fix: Skip update if deprecated endpoint still present
    
    * lint: Ignore Rspec/AnyInstance rule in specs
    
    * ref: Fix CyclomaticComplexity in 'run' method
    
    * fix: Record is saved in transaction once per record

--- app/services/decidim/repair_url_in_content_service.rb
+++ app/services/decidim/repair_url_in_content_service.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 module Decidim
+  # Looks for any occurence of "@deprecated_endpoint" in every database columns of type COLUMN_TYPES
+  # For each field containing @deprecated_endpoint:
+  #   - Looks for the current ActiveStorage::Blob with the given filename
+  #   - Find the blob's service_url
+  #   - Replace the @deprecated_endpoint with the blob's service_url in text
+  #   - Update the column
+  # Context:
+  # After S3 assets migration with rake task "bundle exec rake scaleway:storage:migrate_from_local", every linked documents URL were well updated.
+  # However every links added to text fields redirecting to an uploaded file were outdated and still redirects to the old S3 bucket
   class RepairUrlInContentService
     COLUMN_TYPES = [:string, :jsonb, :text].freeze
-    OLD_URL = "https://villeurbanne-prod-storage.s3.fr-par.scw.cloud"
+    DEFAULT_LOGGER = Rails.logger
 
-    def self.run
-      new.run
+    # TODO: must work with image src
+    # @param [String] deprecated_endpoint
+    def self.run(deprecated_endpoint)
+      new(deprecated_endpoint).run
+    end
+
+    # @param [String] deprecated_endpoint
+    def initialize(deprecated_endpoint, logger = nil)
+      @logger = logger || DEFAULT_LOGGER
+      @deprecated_endpoint = deprecated_endpoint
     end
 
     def run
@@ -14,30 +31,97 @@ module Decidim
       # For each model, find all records that have a column of type string jsonb or text
       # For each record, replace all urls contained in content with the new url
       # Save the record
+      return false if @deprecated_endpoint.blank?
 
-      schema.each do |model, columns|
-        columns.each do |column|
-          records(model, column).each do |record|
-            puts "Updating #{model}##{record.id}##{column.name}"
-            old_content = record.send(column.name)
-            new_content = clean_content(record.send(column.name))
+      models.each do |model|
+        model = model.safe_constantize
+        next unless model.respond_to?(:columns)
 
-            puts "Old content: #{old_content}"
-            puts "New content: #{new_content}"
+        @logger.info("Checking model #{model} for deprecated endpoints #{@deprecated_endpoint}")
+        records = records_for model
+        next if records.blank?
 
-            record.update!(column.name => new_content)
-          end
+        @logger.info "Found #{records.count} records to update for #{model}"
+        records.each do |record|
+          columns = model.columns.select { |column| column.type.in? COLUMN_TYPES }
+          record = update_each_column(record, columns)
+
+          save_record!(record)
         end
       end
+    end
+
+    def save_record!(record)
+      if record.invalid?
+        @logger.warn "Invalid record #{record.class}##{record.id}: #{record.errors.full_messages.join(", ")}"
+        return
+      end
+
+      if record.has_changes_to_save?
+        record.class.transaction do
+          record.save!
+        end
+      else
+        @logger.info "No changes to save for #{record.class}##{record.id}"
+      end
+    end
+
+    # @param [Object] record
+    # @param [[ActiveRecord::ConnectionAdapters::PostgreSQL::Column]] columns
+    # @return record | nil
+    def update_each_column(record, columns)
+      columns.each do |column|
+        current_content = record.send(column.name)
+        next unless current_content.to_s.include?(@deprecated_endpoint)
+
+        @logger.info "Updating ##{record.class}##{record.id}.#{column.name}"
+        new_content = clean_content(record.send(column.name))
+
+        @logger.info "Old content: #{current_content}"
+        @logger.info "New content: #{new_content}"
+
+        if new_content.to_s.include?(@deprecated_endpoint)
+          @logger.warn "New content '#{record.class}##{record.id}.#{column.name}' still contains deprecated endpoint #{@deprecated_endpoint}"
+          next
+        end
+
+        record.write_attribute(:"#{column.name}", new_content)
+      end
+
+      record
+    end
+
+    def records_for(model)
+      model.columns.map do |col|
+        next unless col.type.in?(COLUMN_TYPES)
+
+        model.where("#{col.name}::text LIKE ?", "%#{@deprecated_endpoint}%")
+      end.compact.reduce(&:or)
+    rescue StandardError => e
+      @logger.warn "Error while fetching records from #{model}: #{e.message}"
+      []
+    end
+
+    # @return [String]
+    def models
+      ActiveRecord::Base.connection.tables.map do |table|
+        next unless table.starts_with?("decidim_")
+
+        table.tr("_", "/").classify
+      end.compact
+    end
+
+    def blobs
+      @blobs ||= ActiveStorage::Blob.pluck(:filename, :id)
     end
 
     def clean_content(content)
       content.transform_values do |value|
         Nokogiri::HTML(value).tap do |doc|
           doc.css("a").each do |link|
-            next unless link["href"].include?(OLD_URL)
+            next unless link["href"].include?(@deprecated_endpoint)
 
-            puts "Replacing #{link["href"]} with #{link["href"]}"
+            @logger.info "Replacing #{link["href"]} with #{link["href"]}"
             link["href"] = new_link(link["href"])
           end
         end.css("body").inner_html
@@ -47,34 +131,13 @@ module Decidim
     def new_link(link)
       uri = URI.parse(link)
       filename = CGI.parse(uri.query)["response-content-disposition"].first.match(/filename=("?)(.+)\1/)[2]
+      _filename, id = blobs.select { |blob, _id| ActiveSupport::Inflector.transliterate(blob) == filename }.first
 
-      _file_name, id = blobs.select do |blob, _id|
-        ActiveSupport::Inflector.transliterate(blob) == filename
-      end.first
-
-      ActiveStorage::Blob.find(id).service_url
+      find_service_url_for_blob(id)
     end
 
-    def records(model, column)
-      model.where("#{column.name}::text LIKE ?", "%#{OLD_URL}%")
-    end
-
-    def schema
-      models.index_with do |model|
-        model.columns.select { |column| COLUMN_TYPES.include?(column.type) }
-      end
-    end
-
-    def models
-      ActiveRecord::Base.connection.tables.map do |table|
-        next unless table.starts_with?("decidim_")
-
-        table.tr("_", "/").classify.safe_constantize
-      end.compact
-    end
-
-    def blobs
-      @blobs ||= ActiveStorage::Blob.pluck(:filename, :id)
+    def find_service_url_for_blob(blob_id)
+      ActiveStorage::Blob.find(blob_id).service_url
     end
   end
 end
---
+++ spec/services/decidim/repair_url_in_content_service_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::RepairUrlInContentService do
+  subject { described_class.run(deprecated_endpoint) }
+
+  let(:deprecated_endpoint) { "s3.decidim.org" }
+  let(:valid_endpoint) { "s3.valid.org" }
+  let(:connection_tables) { %w(schema_migrations decidim_comments_comments decidim_proposals_proposals ar_internal_metadata) }
+  let(:comment_1) { create(:comment, body: invalid_body_comment) }
+  let(:comment_2) { create(:comment) }
+  let(:invalid_body_comment) { { en: "<p>Here is a not valid comment with <a href='#{deprecated_url}'>Link text</a></p>" } }
+  let(:deprecated_url) { "https://#{deprecated_endpoint}/xxxx?response-content-disposition=inline%3Bfilename%3D\"BuPa23_reglement-interieur.pdf\"%3Bfilename*%3DUTF-8''BuPa23_r%25C3%25A8glement-int%25C3%25A9rieur.pdf&response-content-type=application%2Fpdf" }
+  let(:valid_url) { "https://#{valid_endpoint}/xxxx?response-content-disposition=inline%3Bfilename%3D\"BuPa23_reglement-interieur.pdf\"%3Bfilename*%3DUTF-8''BuPa23_r%25C3%25A8glement-int%25C3%25A9rieur.pdf&response-content-type=application%2Fpdf" }
+
+  # rubocop:disable RSpec/AnyInstance
+  before do
+    allow(ActiveRecord::Base.connection).to receive(:tables).and_return(connection_tables)
+    allow(Decidim::RepairUrlInContentService).to receive(:models).and_return(["Decidim::Comments::Comment", "Decidim::Proposals::Proposal"])
+    allow_any_instance_of(Decidim::RepairUrlInContentService).to receive(:blobs).and_return([["BuPa23_reglement-interieur.pdf", comment_1.id]])
+    allow_any_instance_of(Decidim::RepairUrlInContentService).to receive(:find_service_url_for_blob).with(comment_1.id).and_return(valid_url)
+  end
+  # rubocop:enable RSpec/AnyInstance
+
+  it "updates values from comments" do
+    expect do
+      subject
+      comment_1.reload
+    end.to change(comment_1, :body)
+
+    expect(comment_1.body["en"]).to include(valid_endpoint)
+  end
+
+  context "when deprecated_endpoint is blank" do
+    let(:deprecated_endpoint) { nil }
+
+    it "returns false" do
+      expect(subject).to be_falsey
+    end
+  end
+
+  context "when resource text is not HTML" do
+    let(:invalid_body_comment) { { en: "Here is a not valid comment with #{deprecated_url}" } }
+
+    it "does not update resources" do
+      expect do
+        subject
+        comment_1.reload
+      end.not_to change(comment_1, :body)
+    end
+  end
+
+  describe "#models" do
+    subject { described_class.new(deprecated_endpoint) }
+
+    before do
+      allow(ActiveRecord::Base.connection).to receive(:tables).and_return(connection_tables)
+    end
+
+    it "only returns Decidim objects as String" do
+      expect(subject.models).to eq(["Decidim::Comments::Comment", "Decidim::Proposals::Proposal"])
+    end
+  end
+
+  describe "#records_for" do
+    subject { described_class.new(deprecated_endpoint).records_for(model) }
+
+    let(:model) { Decidim::Comments::Comment }
+
+    it "returns all records that have a column of type string jsonb or text" do
+      expect(subject).to include(comment_1)
+      expect(subject).not_to include(comment_2)
+    end
+
+    it "generates a unique SQL query" do
+      expect(subject.to_sql).to eq("SELECT \"decidim_comments_comments\".* FROM \"decidim_comments_comments\" WHERE (((((decidim_commentable_type::text LIKE '%#{deprecated_endpoint}%') OR (decidim_root_commentable_type::text LIKE '%#{deprecated_endpoint}%')) OR (decidim_author_type::text LIKE '%#{deprecated_endpoint}%')) OR (body::text LIKE '%#{deprecated_endpoint}%')) OR (decidim_participatory_space_type::text LIKE '%#{deprecated_endpoint}%'))")
+    end
+
+    context "when model cannot be constantized" do
+      let(:model) { "Decidim::Comments::NotExistingModel" }
+
+      it "returns an empty array" do
+        expect(subject).to eq([])
+      end
+    end
+  end
+end
