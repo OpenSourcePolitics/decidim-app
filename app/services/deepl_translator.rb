2430ab313125e7346d8f2bdc4a55c0b8eb1bea35
armandfardeau <fardeauarmand@gmail.com>
2023-08-09 14:22:37 +0200

    Fix deepl translator

--- app/services/deepl_translator.rb
+++ app/services/deepl_translator.rb
@@ -12,13 +12,27 @@ class DeeplTranslator
   end
 
   def translate
-    translation = DeepL.translate text, source_locale, target_locale
+    if translatable_locale?(target_locale)
+      translation = DeepL.translate text, source_locale, target_locale
 
-    Decidim::MachineTranslationSaveJob.perform_later(
-      resource,
-      field_name,
-      target_locale,
-      translation.text
-    )
+      Decidim::MachineTranslationSaveJob.perform_later(
+        resource,
+        field_name,
+        target_locale,
+        translation.text
+      )
+    else
+      Rails.logger.info "DeepL: #{target_locale} is not a translatable locale"
+    end
+  end
+
+  def translatable_locale?(locale)
+    deepl_translatable_locales.include?(locale)
+  end
+
+  def deepl_translatable_locales
+    Rails.cache.fetch("deepl_translatable_locales", expires_in: 1.day) do
+      DeepL.languages.map(&:code).map(&:downcase)
+    end
   end
 end
