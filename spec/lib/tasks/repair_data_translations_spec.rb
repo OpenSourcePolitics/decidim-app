8ae72e730d7d3f40dece69c3d52e172f4f465228
armandfardeau <fardeauarmand@gmail.com>
2023-08-16 15:40:57 +0200

    Allow repair translation to be deactivated

--- spec/lib/tasks/repair_data_translations_spec.rb
+++ spec/lib/tasks/repair_data_translations_spec.rb
@@ -5,10 +5,12 @@ require "spec_helper"
 describe "rake decidim:repair:translations", type: :task do
   let(:task) { Rake::Task[:"decidim:repair:translations"] }
   let!(:comment) { create(:comment) }
+  let(:enable_machine_translations) { true }
 
   before do
     clear_enqueued_jobs
     clear_performed_jobs
+    allow(Decidim).to receive(:enable_machine_translations).and_return(enable_machine_translations)
   end
 
   after do
@@ -24,5 +26,15 @@ describe "rake decidim:repair:translations", type: :task do
     expect(Decidim::RepairTranslationsService).to receive(:run).and_return([[Decidim::Comments::Comment, comment.id]])
 
     task.execute
+  end
+
+  context "when translation is not activated" do
+    let(:enable_machine_translations) { false }
+
+    it "doesn't calls the service" do
+      expect(Decidim::RepairTranslationsService).not_to receive(:run)
+
+      task.execute
+    end
   end
 end
