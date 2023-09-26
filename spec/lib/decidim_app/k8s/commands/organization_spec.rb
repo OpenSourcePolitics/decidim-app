f9130fd06df3c7926fd059d3c2a095ea115ee7ec
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-09-26 13:44:18 +0200

    fix: k8s specs

--- spec/lib/decidim_app/k8s/commands/organization_spec.rb
+++ spec/lib/decidim_app/k8s/commands/organization_spec.rb
@@ -148,7 +148,7 @@ describe DecidimApp::K8s::Commands::Organization do
         subject.subscribe(listener)
 
         expect do
-          expect(subject.call).to be_a(::Rectify::Command)
+          expect(subject.call).to be_a(Decidim::Command)
         end.to not_change(Decidim::Organization, :count)
       end
 
@@ -216,7 +216,7 @@ describe DecidimApp::K8s::Commands::Organization do
           expect(smtp_settings["address"]).to eq("address.smtp.org")
           expect(smtp_settings["port"]).to eq(8080)
           expect(smtp_settings["authentication"]).to eq("plain")
-          expect(smtp_settings["enable_starttls_auto"]).to eq(true)
+          expect(smtp_settings["enable_starttls_auto"]).to be(true)
           expect(Decidim::AttributeEncryptor.decrypt(smtp_settings["encrypted_password"])).to eq("password")
         end
 
@@ -226,7 +226,7 @@ describe DecidimApp::K8s::Commands::Organization do
           expect(Decidim::AttributeEncryptor.decrypt(omniauth_settings["omniauth_settings_publik_client_id"])).to eq("12345")
           expect(Decidim::AttributeEncryptor.decrypt(omniauth_settings["omniauth_settings_publik_client_secret"])).to eq("12345")
           expect(Decidim::AttributeEncryptor.decrypt(omniauth_settings["omniauth_settings_publik_site_url"])).to eq("https://example.com/")
-          expect(omniauth_settings["omniauth_settings_publik_enabled"]).to eq(true)
+          expect(omniauth_settings["omniauth_settings_publik_enabled"]).to be(true)
         end
       end
 
