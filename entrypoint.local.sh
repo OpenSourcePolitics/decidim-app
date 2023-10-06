4a6630fa7671381d942befd859237dbe4c3b0d22
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-10-06 15:44:42 +0200

    refactor: Remove not needed entrypoint.local.sh

--- entrypoint.local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-bundle exec rake db:create decidim_app:k8s:install
-bundle exec rails server -b "ssl://0.0.0.0:3000?key=/app/certificate-https-local/key.pem&cert=/app/certificate-https-local/cert.pem"
