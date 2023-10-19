d2cf62bcb87b229645ecbde88c137a9e9dd0f099
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-10-19 12:07:14 +0200

    lint: Fix rubocop offenses

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,16 +6,8 @@ require "decidim_app/rack_attack/fail2ban"
 
 # Enabled by default in production
 # Can be deactivated with 'ENABLE_RACK_ATTACK=0'
-# frozen_string_literal: true
-
-require "decidim_app/rack_attack"
-require "decidim_app/rack_attack/throttling"
-require "decidim_app/rack_attack/fail2ban"
-
 DecidimApp::RackAttack.deactivate_decidim_throttling!
 
-# Enabled by default in production
-# Can be deactivated with 'ENABLE_RACK_ATTACK=0'
 if DecidimApp::RackAttack.rack_enabled?
   DecidimApp::RackAttack.enable_rack_attack!
   DecidimApp::RackAttack.apply_configuration
