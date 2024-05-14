284c5ede60056acbe9c875d962bd627cb08904b3
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-05-14 16:35:21 +0200

    fix: Configure Half Signup

---
+++ config/initializers/half_signup.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Decidim::HalfSignup.configure do |config|
+  config.show_tos_page_after_signup = Rails.application.secrets.dig(:decidim, :half_signup, :show_tos_page_after_signup)
+  config.auth_code_length = 4
+  config.default_countries = ENV.fetch("AVAILABLE_LOCALES", "fr").split(",").map(&:to_sym)
+end
