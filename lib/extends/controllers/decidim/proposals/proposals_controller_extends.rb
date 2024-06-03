37172e809815b2542cef68d03e624f7fbcee9b1a
stephanie rousset <strabon@club-internet.fr>
2024-06-03 16:47:45 +0200

    refactor: update extends after review

--- lib/extends/controllers/decidim/proposals/proposals_controller_extends.rb
+++ lib/extends/controllers/decidim/proposals/proposals_controller_extends.rb
@@ -9,19 +9,22 @@ module ProposalsControllerExtends
     def default_filter_scope_params
       return "all" unless current_component.scopes.any?
 
-      if (scope = current_component.scope)
-        scope_children = scope.children
-        scope_children_ids = scope_children.present? ? scope_children.map(&:id) : []
-        ids = ["all", scope.id] + scope_children_ids
-        while Decidim::Scope.where(parent_id: scope_children_ids).present?
-          sub_ids = Decidim::Scope.where(parent_id: scope_children_ids).pluck(:id)
-          ids += sub_ids
-          scope_children_ids = sub_ids
-        end
-        ids.map(&:to_s)
-      else
-        %w(all global) + current_component.scopes.pluck(:id).map(&:to_s)
+      scope = current_component.scope
+      return current_component_scope_ids if scope.blank?
+
+      scope_children_ids = scope.children&.map(&:id)
+      ids = ["all", scope.id] + scope_children_ids
+      while Decidim::Scope.where(parent_id: scope_children_ids).present?
+        sub_ids = Decidim::Scope.where(parent_id: scope_children_ids).pluck(:id)
+        ids += sub_ids
+        scope_children_ids = sub_ids
       end
+      ids.map(&:to_s)
+    end
+
+    def current_component_scope_ids
+      component_scopes = current_component.scopes.pluck(:id).map(&:to_s)
+      %w(all global) + component_scopes
     end
   end
 end
