69ae2f6a35b2c5472ad700d069964bb15c132568
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-07-24 12:48:18 +0200

    feat(sidekiq): Add env var on configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,8 @@
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    network_timeout: ENV.fetch("REDIS_NETWORK_TIMEOUT", 0),
-    pool_timeout: ENV.fetch("REDIS_POOL_TIMEOUT", 0),
-    reconnect_attempts: ENV.fetch("REDIS_RECONNECT_ATTEMPTS", 0)
+    network_timeout: ENV.fetch("REDIS_NETWORK_TIMEOUT", 10),
+    pool_timeout: ENV.fetch("REDIS_POOL_TIMEOUT", 10),
+    reconnect_attempts: ENV.fetch("REDIS_RECONNECT_ATTEMPTS", 1)
   }
 end
