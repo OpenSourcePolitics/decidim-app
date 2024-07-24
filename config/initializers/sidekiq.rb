35bb038ed5d60bca88f10ca3a1502ce3e4c87df0
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-07-24 12:32:08 +0200

    feat(redis): Allow to configure Redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,8 @@
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    network_timeout: 5,
-    pool_timeout: 5,
+    network_timeout: ENV.fetch("REDIS_NETWORK_TIMEOUT", 0),
+    pool_timeout: ENV.fetch("REDIS_POOL_TIMEOUT", 0),
     reconnect_attempts: ENV.fetch("REDIS_RECONNECT_ATTEMPTS", 0)
   }
 end
