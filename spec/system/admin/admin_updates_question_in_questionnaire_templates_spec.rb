44c72859274595155d18d33e16ba426a509a5134
stephanie rousset <strabon@club-internet.fr>
2024-09-16 10:18:45 +0200

    refactor: update with rubocop

--- spec/system/admin/admin_updates_question_in_questionnaire_templates_spec.rb
+++ spec/system/admin/admin_updates_question_in_questionnaire_templates_spec.rb
@@ -21,16 +21,13 @@ describe "Admin adds display condition to template's questionniaire question", t
 
     visit decidim_admin_templates.edit_questionnaire_path(template.id)
     # expand question two
-    save_and_open_page
     find("[data-toggle$=button--expand-question-questionnaire_question_#{question_two.id}]").click
-    #within "#questionnaire_question_#{question_two.id}-field" do
-      # add display condition
-      find(".add-display-condition").click
-      # select question
-      select translated(question_one.body), from: "questionnaire[questions][#{question_two.id}][display_conditions][questionnaire-display-condition-id][decidim_condition_question_id]"
-      # select equal
-      select "Equal", from: "questionnaire[questions][#{question_two.id}][display_conditions][questionnaire-display-condition-id][condition_type]"
-    #end
+    # add display condition
+    find(".add-display-condition").click
+    # select question
+    select translated(question_one.body), from: "questionnaire[questions][#{question_two.id}][display_conditions][questionnaire-display-condition-id][decidim_condition_question_id]"
+    # select equal
+    select "Equal", from: "questionnaire[questions][#{question_two.id}][display_conditions][questionnaire-display-condition-id][condition_type]"
     # validate we have the 2 answer options from question one in the select
     select = find("#questionnaire_questions_#{question_two.id}_display_conditions_questionnaire-display-condition-id_decidim_answer_option_id")
     expect(select).to have_content(translated(question_one.answer_options.first.body))
