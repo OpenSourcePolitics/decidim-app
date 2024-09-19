bce1c1f7e65b0c77f925e6a14f58f1da671c3343
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-09-19 15:07:50 +0200

    feat(Sidekiq): Load Sidekiq route

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+
+return unless const_defined?(Sidekiq)
+
+Rails.logger.warn "Configuring Sidekiq..."
+
 Sidekiq.configure_client do |config|
   config.redis = {
     network_timeout: 10,
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
+require "sidekiq/web"
+require "sidekiq-scheduler/web"
+
 Rails.application.routes.draw do
+  authenticate :admin do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   mount Decidim::Core::Engine => '/'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
