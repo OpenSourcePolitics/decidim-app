1561bd0f946a26a06f90a700a1836f5279e63c0f
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-10-08 14:11:11 +0200

    fix: Drupal importers

--- app/models/drupal/abstract_record.rb
+++ app/models/drupal/abstract_record.rb
@@ -3,7 +3,6 @@
 module Drupal
   class AbstractRecord < ApplicationRecord
     self.abstract_class = true
-    establish_connection :drupal
 
     def self.instance_method_already_implemented?(method_name)
       return true if %w(changed changed?).include? method_name
