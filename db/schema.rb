18462e2450d1904601e94b131672793d00c3f534
AyakorK <guillaume.moret@yahoo.com>
2024-10-10 11:43:17 +0200

    fix: Fix migration that has been changed since first implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_09_105927) do
+ActiveRecord::Schema.define(version: 2024_10_10_092645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
