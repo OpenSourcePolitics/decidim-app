591f4ef8b42bea990f7c79ad69bbba19ae06d5f4
barbara oliveira <barbarafictiovore@gmail.com>
2024-10-16 16:12:19 +0200

    lint code

--- app/jobs/private_body_decrypt_job.rb
+++ app/jobs/private_body_decrypt_job.rb
@@ -3,19 +3,16 @@
 class PrivateBodyDecryptJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform
+    extra_fields = Decidim::DecidimAwesome::ProposalExtraField.where(decrypted_private_body: nil).where.not(private_body: nil)
+    return unless extra_fields.any?
 
-    extra_fields = Decidim::DecidimAwesome::ProposalExtraField
-                     .where(decrypted_private_body: nil)
-                     .where.not(private_body: nil)
-    if extra_fields.any?
-      Rails.logger "Extra fields to update: #{extra_fields.size}"
-      count = 0
-      extra_fields.find_each do |extra_field|
-        extra_field.update(decrypted_private_body: extra_field.private_body.to_s)
-        count += 1 if extra_field.decrypted_private_body_previous_change.present?
-      end
-      Rails.logger "Extra fields updated: #{count}"
+    Rails.logger.info "Extra fields to update: #{extra_fields.size}"
+    count = 0
+    extra_fields.find_each do |extra_field|
+      extra_field.update(decrypted_private_body: extra_field.private_body.to_s)
+      count += 1 if extra_field.decrypted_private_body_previous_change.present?
     end
+    Rails.logger.info "Extra fields updated: #{count}"
   end
 end
