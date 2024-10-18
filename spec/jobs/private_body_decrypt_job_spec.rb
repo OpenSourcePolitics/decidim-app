8ca80382fe9bdeb06c9f1de9f4880bdc42cd8f8a
barbara oliveira <barbarafictiovore@gmail.com>
2024-10-18 17:12:42 +0200

    update spec

--- spec/jobs/private_body_decrypt_job_spec.rb
+++ spec/jobs/private_body_decrypt_job_spec.rb
@@ -2,8 +2,7 @@
 
 require "spec_helper"
 
-describe "rake decidim:private_body_decrypted_job", type: :task do
-  let(:task) { Rake::Task["decidim:set_decrypted_private_body"] }
+describe PrivateBodyDecryptJob, type: :job do
   let!(:extra_fields) { create(:awesome_proposal_extra_fields, proposal: create(:extended_proposal)) }
 
   before do
@@ -13,22 +12,20 @@ describe "rake decidim:private_body_decrypted_job", type: :task do
     allow(Rails.logger).to receive(:info)
   end
 
-  it "preloads the Rails environment" do
-    expect(task.prerequisites).to include "environment"
-  end
-
-  it "prints nothing if no extra_fields to update" do
-    expect { task.execute }.not_to output.to_stdout
-  end
-
-  it "sets the decrypted body correctly if there is a private body" do
-    # we need an empty decrypted_private_body to test if the task will update it well
-    allow_any_instance_of(Decidim::DecidimAwesome::ProposalExtraField).to receive(:update_decrypted_body)
-    extra_fields.update(decrypted_private_body: nil)
+  it "logs and updates the decrypted body if extra fields are present" do
+    extra_fields.update_columns(decrypted_private_body: nil)
     expect(extra_fields.decrypted_private_body).to be_nil
     expect(Rails.logger).to receive(:info).with("Extra fields to update: 1")
     expect(Rails.logger).to receive(:info).with("Extra fields updated: 1")
-    PrivateBodyDecryptJob.perform_now
+    described_class.perform_now
     expect(extra_fields.reload.decrypted_private_body).to eq('{"en"=>"<xml><dl><dt name=\"something\">Something</dt></dl></xml>"}')
+  end
+
+  it "does not log or update if no extra fields are present" do
+    extra_fields.update(decrypted_private_body: extra_fields.private_body.to_s)
+    expect(extra_fields.reload.decrypted_private_body).not_to be_nil
+    expect(Rails.logger).not_to receive(:info).with("Extra fields to update: 1")
+    expect(Rails.logger).not_to receive(:info).with("Extra fields updated: 1")
+    described_class.perform_now
   end
 end
