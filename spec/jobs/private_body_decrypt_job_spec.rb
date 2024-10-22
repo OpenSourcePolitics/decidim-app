dae050effde9f781dee9d3e474e04bab4370dc9c
barbara oliveira <barbarafictiovore@gmail.com>
2024-10-22 11:08:47 +0200

    update syntax test with a context instade of only 'it'

--- spec/jobs/private_body_decrypt_job_spec.rb
+++ spec/jobs/private_body_decrypt_job_spec.rb
@@ -12,23 +12,27 @@ describe PrivateBodyDecryptJob, type: :job do
     allow(Rails.logger).to receive(:info)
   end
 
-  it "logs and updates the decrypted body if extra fields are present" do
-    # rubocop:disable Rails/SkipsModelValidations
-    extra_fields.update_columns(decrypted_private_body: nil)
-    # rubocop:enable Rails/SkipsModelValidations
-    expect(extra_fields.decrypted_private_body).to be_nil
-    expect(Rails.logger).to receive(:info).with("Extra fields to update: 1")
-    expect(Rails.logger).to receive(:info).with("Extra fields updated: 1")
-    described_class.perform_now
-    expect(extra_fields.reload.decrypted_private_body).to eq('{"en"=>"<xml><dl><dt name=\"something\">Something</dt></dl></xml>"}')
+  context "when extra fields are present" do
+    it "logs and updates the decrypted body" do
+      # rubocop:disable Rails/SkipsModelValidations
+      extra_fields.update_columns(decrypted_private_body: nil)
+      # rubocop:enable Rails/SkipsModelValidations
+      expect(extra_fields.decrypted_private_body).to be_nil
+      expect(Rails.logger).to receive(:info).with("Extra fields to update: 1")
+      expect(Rails.logger).to receive(:info).with("Extra fields updated: 1")
+      described_class.perform_now
+      expect(extra_fields.reload.decrypted_private_body).to eq('{"en"=>"<xml><dl><dt name=\"something\">Something</dt></dl></xml>"}')
+    end
   end
 
-  it "does not log or update if no extra fields are present" do
-    extra_fields.update(decrypted_private_body: extra_fields.private_body.to_s)
-    expect(extra_fields.reload.decrypted_private_body).not_to be_nil
-    expect(Rails.logger).not_to receive(:info).with("Extra fields to update: 1")
-    expect(Rails.logger).not_to receive(:info).with("Extra fields updated: 1")
-    described_class.perform_now
-    expect(extra_fields.reload.decrypted_private_body).to eq(extra_fields.private_body.to_s)
+  context "when extra fields are missing" do
+    it "does not log or update" do
+      extra_fields.update(decrypted_private_body: extra_fields.private_body.to_s)
+      expect(extra_fields.reload.decrypted_private_body).not_to be_nil
+      expect(Rails.logger).not_to receive(:info).with("Extra fields to update: 1")
+      expect(Rails.logger).not_to receive(:info).with("Extra fields updated: 1")
+      described_class.perform_now
+      expect(extra_fields.reload.decrypted_private_body).to eq(extra_fields.private_body.to_s)
+    end
   end
 end
