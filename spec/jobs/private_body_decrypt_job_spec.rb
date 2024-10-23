5e514372528be4f5876686eab3cc32238b584903
barbara oliveira <barbarafictiovore@gmail.com>
2024-10-23 12:27:02 +0200

    add more context in spec file

--- spec/jobs/private_body_decrypt_job_spec.rb
+++ spec/jobs/private_body_decrypt_job_spec.rb
@@ -3,35 +3,57 @@
 require "spec_helper"
 
 describe PrivateBodyDecryptJob, type: :job do
-  let!(:extra_fields) { create(:awesome_proposal_extra_fields, proposal: create(:extended_proposal)) }
+  let!(:proposal_with_private_body) { create(:awesome_proposal_extra_fields, proposal: create(:extended_proposal), private_body: '<xml><dl><dt name="something">Something</dt></dl></xml>', decrypted_private_body: nil) }
+  let!(:proposal_without_private_body) { create(:awesome_proposal_extra_fields, proposal: create(:extended_proposal), private_body: nil, decrypted_private_body: nil) }
+  let!(:proposal_with_both_bodies_defined) { create(:awesome_proposal_extra_fields, proposal: create(:extended_proposal), private_body: '<xml><dl><dt name="something">Something</dt></dl></xml>', decrypted_private_body: '<xml><dl><dt name="something">Something</dt></dl></xml>') }
+  let!(:proposal_without_extra_fields) { create(:awesome_proposal_extra_fields, proposal: create(:extended_proposal)) }
 
   before do
-    extra_fields.private_body = { "en" => '<xml><dl><dt name="something">Something</dt></dl></xml>' }
-    extra_fields.save!
-
     allow(Rails.logger).to receive(:info)
   end
 
   context "when extra fields are present" do
-    it "logs and updates the decrypted body" do
-      # rubocop:disable Rails/SkipsModelValidations
-      extra_fields.update_columns(decrypted_private_body: nil)
-      # rubocop:enable Rails/SkipsModelValidations
-      expect(extra_fields.decrypted_private_body).to be_nil
-      expect(Rails.logger).to receive(:info).with("Extra fields to update: 1")
-      expect(Rails.logger).to receive(:info).with("Extra fields updated: 1")
-      described_class.perform_now
-      expect(extra_fields.reload.decrypted_private_body).to eq('{"en"=>"<xml><dl><dt name=\"something\">Something</dt></dl></xml>"}')
+    context "with private body defined and decrypted_private_body nil" do
+      it "logs and updates the decrypted body" do
+        # rubocop:disable Rails/SkipsModelValidations
+        proposal_with_private_body.update_columns(decrypted_private_body: nil)
+        # rubocop:enable Rails/SkipsModelValidations
+        expect(proposal_with_private_body.decrypted_private_body).to be_nil
+        described_class.perform_now
+        expect(proposal_with_private_body.reload.decrypted_private_body).to eq('<xml><dl><dt name="something">Something</dt></dl></xml>')
+      end
     end
+
+    context "with private body nil, and decrypted_private_body nil" do
+      it "does not log or update anything" do
+        expect(proposal_without_private_body.decrypted_private_body).to be_nil
+        expect(proposal_without_private_body.private_body).to be_nil
+        expect(Rails.logger).not_to receive(:info).with("Extra fields to update: 1")
+        expect(Rails.logger).not_to receive(:info).with("Extra fields updated: 1")
+        described_class.perform_now
+        expect(proposal_without_private_body.reload.decrypted_private_body).to be_nil
+      end
+    end
+
+    context "with private body and decrypted_private_body defined" do
+      it "does not log or update anything" do
+        expect(proposal_with_both_bodies_defined.decrypted_private_body).not_to be_nil
+        expect(proposal_with_both_bodies_defined.private_body).not_to be_nil
+        expect(Rails.logger).not_to receive(:info).with("Extra fields to update: 1")
+        expect(Rails.logger).not_to receive(:info).with("Extra fields updated: 1")
+        described_class.perform_now
+        expect(proposal_with_both_bodies_defined.reload.decrypted_private_body).to eq('<xml><dl><dt name="something">Something</dt></dl></xml>')
+      end
+    end
+
   end
 
   context "when extra fields are missing" do
     it "does not log or update" do
-      expect(extra_fields.reload.decrypted_private_body).not_to be_nil
       expect(Rails.logger).not_to receive(:info).with("Extra fields to update: 1")
       expect(Rails.logger).not_to receive(:info).with("Extra fields updated: 1")
       described_class.perform_now
-      expect(extra_fields.reload.decrypted_private_body).to eq('{"en"=>"<xml><dl><dt name=\"something\">Something</dt></dl></xml>"}')
+      expect(proposal_without_extra_fields.reload.decrypted_private_body).to eq(nil)
     end
   end
 end
