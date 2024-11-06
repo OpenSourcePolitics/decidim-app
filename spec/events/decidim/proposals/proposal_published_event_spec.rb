a28f39a0678348faa237a343fafa5b56bbe87aa1
barbara oliveira <barbarafictiovore@gmail.com>
2024-11-06 13:46:25 +0100

    start test rspec

---
+++ spec/events/decidim/proposals/proposal_published_event_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim::Proposals
+  describe "ProposalPublishedEvent", type: :job do
+    include MailerHelpers
+
+    let(:organization) { create(:organization) }
+    let(:participatory_process) { create(:participatory_process, organization: organization) }
+    let(:component) { create(:component, manifest_name: :proposals, participatory_space: participatory_process) }
+    let!(:proposal_state) { create(:proposal_state, component: component) }
+    let(:proposal) { create(:proposal, component: component, proposal_state: proposal_state, title: { en: "Test proposition notification" }) }
+    let(:user) { create(:user, :admin, organization: organization, notifications_sending_frequency: "daily") }
+
+    before do
+      clear_emails
+    end
+
+    context "when proposal is published" do
+      it "sends a notification email with the correct content" do
+        Decidim::EventsManager.publish(
+          event: "decidim.proposals.proposal_published",
+          event_class: ProposalPublishedEvent,
+          resource: proposal,
+          affected_users: [user],
+        )
+
+        perform_enqueued_jobs do
+          Decidim::EmailNotificationsDigestGeneratorJob.perform_now(user.id, user.notifications_sending_frequency)
+        end
+
+        expect(last_email).not_to be_nil
+        expect(last_email_body).not_to include("translation missing")
+        expect(last_email_body).to include("Test proposition notification")
+
+      end
+    end
+
+  end
+end
+#
+# shared_context "when sends the notification digest" do
+#   context "when daily notification mail" do
+#     let(:user) { create(:user, :admin, organization:, notifications_sending_frequency: "daily") }
+#
+#     it_behaves_like "notification digest mail"
+#   end
+#
+#   context "when weekly notification mail" do
+#     let(:user) { create(:user, :admin, organization:, notifications_sending_frequency: "weekly") }
+#
+#     it_behaves_like "notification digest mail"
+#   end
+# end
+#
+# shared_examples_for "notification digest mail" do
+#   context "when a notifiable event takes place" do
+#     let!(:organization) { create(:organization) }
+#     let!(:participatory_space) { create(:participatory_process, organization:) }
+#
+#     it "sends a notification to the user's email" do
+#       perform_enqueued_jobs do
+#         expect(command.call).to broadcast(:ok)
+#         Decidim::Notification.last.update(created_at: 1.day.ago)
+#         Decidim::EmailNotificationsDigestGeneratorJob.perform_now(user.id, user.notifications_sending_frequency)
+#       end
+#
+#       expect(last_email_body.length).to be_positive
+#       expect(last_email_body).not_to include("translation missing")
+#     end
+#   end
+# end
