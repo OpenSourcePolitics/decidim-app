cb8772af1b552efdbf033f89742153272836922f
Quentin Champenois <quentin.champenois7@gmail.com>
2024-11-07 17:06:28 +0100

    fix: Change ProposalPublishedEvent to SimpleEvent

--- app/events/decidim/proposals/proposal_published_event.rb
+++ app/events/decidim/proposals/proposal_published_event.rb
@@ -1,13 +1,7 @@
 # app/events/decidim/proposals/proposal_published_event.rb
 module Decidim
   module Proposals
-    class ProposalPublishedEvent < Decidim::Events::BaseEvent
-      include Decidim::Events::NotificationEvent
-
-      def self.model_name
-        ActiveModel::Name.new(self, nil, I18n.t('decidim.proposals.notifications.proposal_published.subject'))
-      end
-
+    class ProposalPublishedEvent < Decidim::Events::SimpleEvent
       def notification_title
         I18n.t("decidim.proposals.notifications.proposal_published.title", proposal_title: resource_title)
       end
@@ -23,11 +17,7 @@ module Decidim
       end
 
       def resource_path
-        Decidim::Proposals::Engine.routes.url_helpers.proposal_path(
-          resource,
-          component_id: resource.component.id,
-          initiative_slug: resource.component.participatory_space.slug
-        )
+        ""
       end
     end
   end
