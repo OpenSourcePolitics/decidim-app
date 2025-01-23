04173022ed58d0226abe48142d0518ab9ebc5a57
Quentin Champenois <quentin.champenois7@gmail.com>
2025-01-23 18:25:54 +0100

    fix: Remove database migrations

--- config/application.rb
+++ config/application.rb
@@ -3,28 +3,13 @@
 require_relative "boot"
 
 require "decidim/rails"
-
-# Add the frameworks used by your app that are not loaded by Decidim.
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module DecidimApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
