251e551caaf2a761a7005ee625698f592df45bb1
Quentin Champenois <quentin.champenois7@gmail.com>
2025-01-23 18:21:01 +0100

    fix: Rename module to DecidimApp

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DecidimLite
+module DecidimApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
