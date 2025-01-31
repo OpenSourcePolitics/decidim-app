1e98879fe2f9c0137863c6b7276d954d156abaa3
stephanie rousset <strabon@club-internet.fr>
2025-01-31 09:53:25 +0100

    feat: add helper and controllers extends

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ Bundler.require(*Rails.groups)
 module DecidimApp
   class Application < Rails::Application
     config.load_defaults 6.1
+
+    config.after_initialize do
+      # extends
+      require "extends/controllers/decidim/admin/scopes_controller_extends"
+      require "extends/controllers/decidim/scopes_controller_extends"
+      require "extends/helpers/decidim/check_boxes_tree_helper_extends"
+    end
   end
 end
