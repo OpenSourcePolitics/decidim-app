ef678e4d1b7309b200f214ee452b6398e6765848
AyakorK <guillaume.moret@yahoo.com>
2025-02-05 13:04:54 +0100

    lint: Global refactor of the extends to make it more readable

--- lib/extends/controllers/decidim/account_controller_extends.rb
+++ lib/extends/controllers/decidim/account_controller_extends.rb
@@ -7,30 +7,43 @@ module Decidim
       @form = form(Decidim::DeleteAccountForm).from_params(params)
       Decidim::DestroyAccount.call(current_user, @form) do
         on(:ok) do
-          sign_out(current_user)
-          flash[:notice] = t("account.destroy.success", scope: "decidim")
-          if active_omniauth_session?
-            provider = session.delete("omniauth.provider")
-            logout_policy = session.delete("omniauth.#{provider}.logout_policy")
-            logout_path = session.delete("omniauth.#{provider}.logout_path")
-          end
-          if provider.present? && logout_policy == "session.destroy" && logout_path.present?
-            redirect_to omniauth_logout_path(provider, logout_path)
-          elsif active_france_connect_session?
-            destroy_france_connect_session(session["omniauth.france_connect.end_session_uri"])
-          end
+          handle_successful_destruction
         end
         on(:invalid) do
-          flash[:alert] = t("account.destroy.error", scope: "decidim")
-          redirect_to decidim.root_path
+          handle_invalid_destruction
         end
       end
     end
 
     private
 
+    def handle_successful_destruction
+      sign_out(current_user)
+      flash[:notice] = t("account.destroy.success", scope: "decidim")
+      handle_omniauth_logout if active_omniauth_session?
+
+      handle_france_connect_logout if active_france_connect_session?
+    end
+
+    def handle_omniauth_logout
+      provider = session.delete("omniauth.provider")
+      logout_policy = session.delete("omniauth.#{provider}.logout_policy")
+      logout_path = session.delete("omniauth.#{provider}.logout_path")
+
+      redirect_to omniauth_logout_path(provider, logout_path) if provider.present? && logout_policy == "session.destroy" && logout_path.present?
+    end
+
+    def handle_france_connect_logout
+      destroy_france_connect_session(session["omniauth.france_connect.end_session_uri"])
+    end
+
+    def handle_invalid_destruction
+      flash[:alert] = t("account.destroy.error", scope: "decidim")
+      redirect_to decidim.root_path
+    end
+
     def account_params
-      if force_profile_sync_on_omniauth_connection? # Utiliser la méthode de ApplicationHelper
+      if force_profile_sync_on_omniauth_connection?
         params[:user][:name] = current_user.name
         params[:user][:email] = current_user.email
         params[:user][:nickname] = current_user.nickname
