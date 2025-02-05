dda9a396ca9ddd5bf05ae65601b3c1bb7b6b1e33
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-05 15:49:27 +0100

    fix: Add CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,3 +2,16 @@
 
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/develop/en/customize/content_security_policy
+
+Decidim.configure do |config|
+  config.content_security_policies_extra = {
+    "default-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "script-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "style-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "img-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "font-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "connect-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "frame-src" => "localhost:* www.youtube.com fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
+    "media-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io"
+  }
+end
