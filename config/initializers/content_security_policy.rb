9352fdad3a7ea3714f47e5e55de60bd9c26d86a4
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-06 10:24:10 +0100

    fix(core): Add example CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,17 +3,29 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/develop/en/customize/content_security_policy
 
+CSPS = %w(minio:*
+          localhost:*
+          fonts.gstatic.com
+          fonts.googleapis.com
+          decidim.storage.opensourcepolitics.eu
+          club.decidim.opensourcepolitics.eu
+          templates.opensourcepolitics.net
+          unpkg.com
+          www.youtube.com
+          ).freeze
+# tarteaucitron.io
+
 # rubocop:disable Layout/LineLength
 Decidim.configure do |config|
   config.content_security_policies_extra = {
-    "default-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "script-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "style-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "img-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "font-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "connect-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "frame-src" => "localhost:* www.youtube.com fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io",
-    "media-src" => "localhost:* fonts.gstatic.com decidim.storage.opensourcepolitics.eu club.decidim.opensourcepolitics.eu:* templates.opensourcepolitics.net unpkg.com fonts.googleapis.com tarteaucitron.io"
+    "default-src" => CSPS + %w(http://minio:*),
+    "script-src" => CSPS + %w(http://minio:*),
+    "style-src" => CSPS + %w(http://minio:*),
+    "img-src" => CSPS + %w(http://minio:*),
+    "font-src" => CSPS + %w(http://minio:*),
+    "connect-src" => CSPS + %w(http://minio:*),
+    "frame-src" => CSPS + %w(http://minio:*),
+    "media-src" => CSPS + %w(http://minio:)
   }
 end
 # rubocop:enable Layout/LineLength
