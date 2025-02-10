ce0c09d63957fb0bd09ba780f3fc16c012a7e3f0
AyakorK <guillaume.moret@yahoo.com>
2025-02-10 16:48:53 +0100

    fix: Add the skip_first_login_authorization? check to omniauth registrations

---
+++ lib/extends/controllers/decidim/devise/omniauth_registrations_controller_extends.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module OmniauthRegistrationsControllerExtends
+  extend ActiveSupport::Concern
+
+  included do
+    def after_sign_in_path_for(user)
+      if user.present? && user.blocked?
+        check_user_block_status(user)
+      elsif !skip_first_login_authorization? && (first_login_and_not_authorized?(user) && !user.admin? && !pending_redirect?(user))
+        decidim_verifications.first_login_authorizations_path
+      else
+        super
+      end
+    end
+
+    private
+
+    def skip_first_login_authorization?
+      ActiveRecord::Type::Boolean.new.cast(ENV.fetch("SKIP_FIRST_LOGIN_AUTHORIZATION", "false"))
+    end
+  end
+end
+
+Decidim::Devise::OmniauthRegistrationsController.class_eval do
+  include(OmniauthRegistrationsControllerExtends)
+end
