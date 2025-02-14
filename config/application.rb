cd63d233cdd5d1b3c314a795f8eb864b8d743b19
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-14 12:08:00 +0100

    fix: Update rails app

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,16 @@ Bundler.require(*Rails.groups)
 module DecidimApp
   class Application < Rails::Application
     config.load_defaults 6.1
+
+    config.action_dispatch.default_headers = {
+      "X-Frame-Options" => "SAMEORIGIN",
+      "X-XSS-Protection" => "1; mode=block",
+      "X-Content-Type-Options" => "nosniff",
+      "X-Download-Options" => "noopen",
+      "X-Permitted-Cross-Domain-Policies" => "none",
+      "Referrer-Policy" => "strict-origin-when-cross-origin"
+    }
+
+    config.action_dispatch.cookies_serializer = :hybrid
   end
 end
