c46377df6c27a91d205be407155eb283d3b9ef5d
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-14 12:20:04 +0100

    fix(wip): Revert key generator hash from SHA256 to SHA1

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module DecidimApp
     config.load_defaults 7.0
 
     config.action_dispatch.cookies_serializer = :hybrid
+    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
   end
 end
