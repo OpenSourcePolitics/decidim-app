3587634a0477a997a4258ede7a98d81e33c1595f
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-16 23:37:53 +0100

    fix: CSP rules

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,26 +3,24 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/develop/en/customize/content_security_policy
 
-CSPS = %w(minio:*
-          localhost:*
-          fonts.gstatic.com
-          fonts.googleapis.com
-          decidim.storage.opensourcepolitics.eu
-          club.decidim.opensourcepolitics.eu
-          templates.opensourcepolitics.net
-          unpkg.com
-          www.youtube.com).freeze
-# tarteaucitron.io
+provider = Rails.application.secrets.dig(:storage, :provider) || ""
 
-# Decidim.configure do |config|
-#   config.content_security_policies_extra = {
-#     "default-src" => CSPS + %w(http://minio:*),
-#     "script-src" => CSPS + %w(http://minio:*),
-#     "style-src" => CSPS + %w(http://minio:*),
-#     "img-src" => CSPS + %w(http://minio:*),
-#     "font-src" => CSPS + %w(http://minio:*),
-#     "connect-src" => CSPS + %w(http://minio:*),
-#     "frame-src" => CSPS + %w(http://minio:*),
-#     "media-src" => CSPS + %w(http://minio:)
-#   }
-# end
+s3_endpoint = Rails.application.secrets.dig(:storage, provider&.to_sym, :endpoint) if provider.present?
+S3_ENDPOINT = if s3_endpoint&.start_with?("https://")
+                s3_endpoint.split("https://").last
+              else
+                s3_endpoint || ""
+              end
+
+Decidim.configure do |config|
+  config.content_security_policies_extra = {
+    "default-src" => S3_ENDPOINT + %w(templates.opensourcepolitics.net),
+    "img-src" => S3_ENDPOINT,
+    "media-src" => S3_ENDPOINT + %w(www.youtube.com),
+    "script-src" => S3_ENDPOINT + %w(templates.opensourcepolitics.net tarteaucitron.io unpkg.com),
+    "style-src" => S3_ENDPOINT + %w(templates.opensourcepolitics.net),
+    "font-src" => S3_ENDPOINT,
+    "connect-src" => S3_ENDPOINT,
+    "frame-src" => S3_ENDPOINT
+  }
+end
