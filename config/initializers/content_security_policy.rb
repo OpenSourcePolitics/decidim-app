89e66d5da3c6b0dd27559c94bcc3cb5491409e03
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-17 08:53:46 +0100

    fix: Define CSP for localhost

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,15 +3,27 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/develop/en/customize/content_security_policy
 
+CSP = {
+  "default-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net),
+  "img-src" => %w(decidim.storage.opensourcepolitics.eu),
+  "media-src" => %w(decidim.storage.opensourcepolitics.eu www.youtube.com),
+  "script-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net tarteaucitron.io unpkg.com),
+  "style-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net),
+  "font-src" => %w(decidim.storage.opensourcepolitics.eu),
+  "connect-src" => %w(decidim.storage.opensourcepolitics.eu),
+  "frame-src" => %w(decidim.storage.opensourcepolitics.eu)
+}.freeze
+
+if Rails.application.secrets.dig(:storage, :minio, :endpoint).presence == "http://minio:9000"
+  CSP["default-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+  CSP["img-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+  CSP["media-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+  CSP["script-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+  CSP["style-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+  CSP["connect-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+  CSP["frame-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+end
+
 Decidim.configure do |config|
-  config.content_security_policies_extra = {
-    "default-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net),
-    "img-src" => %w(decidim.storage.opensourcepolitics.eu),
-    "media-src" => %w(decidim.storage.opensourcepolitics.eu www.youtube.com),
-    "script-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net tarteaucitron.io unpkg.com),
-    "style-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net),
-    "font-src" => %w(decidim.storage.opensourcepolitics.eu),
-    "connect-src" => %w(decidim.storage.opensourcepolitics.eu),
-    "frame-src" => %w(decidim.storage.opensourcepolitics.eu)
-  }
+  config.content_security_policies_extra = CSP
 end
