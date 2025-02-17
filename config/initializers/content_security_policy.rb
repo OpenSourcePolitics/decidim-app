83b683b3d731aa481c260b8fcaa5122e802fcf22
Quentin Champenois <quentin.champenois7@gmail.com>
2025-02-17 20:20:12 +0100

    refactor: CSP configuration

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,7 +3,7 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/develop/en/customize/content_security_policy
 
-CSP = {
+content_security_policies = {
   "default-src" => %w(decidim.storage.opensourcepolitics.eu templates.opensourcepolitics.net),
   "img-src" => %w(decidim.storage.opensourcepolitics.eu),
   "media-src" => %w(decidim.storage.opensourcepolitics.eu www.youtube.com),
@@ -12,18 +12,19 @@ CSP = {
   "font-src" => %w(decidim.storage.opensourcepolitics.eu),
   "connect-src" => %w(decidim.storage.opensourcepolitics.eu),
   "frame-src" => %w(decidim.storage.opensourcepolitics.eu)
-}.freeze
+}
 
-if Rails.application.secrets.dig(:storage, :minio, :endpoint).presence == "http://minio:9000"
-  CSP["default-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
-  CSP["img-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
-  CSP["media-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
-  CSP["script-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
-  CSP["style-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
-  CSP["connect-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
-  CSP["frame-src"] << Rails.application.secrets.dig(:storage, :minio, :endpoint)
+minio_endpoint = Rails.application.secrets.dig(:storage, :minio, :endpoint)
+if minio_endpoint.presence == "http://minio:9000"
+  content_security_policies["default-src"] << minio_endpoint
+  content_security_policies["img-src"] << minio_endpoint
+  content_security_policies["media-src"] << minio_endpoint
+  content_security_policies["script-src"] << minio_endpoint
+  content_security_policies["style-src"] << minio_endpoint
+  content_security_policies["connect-src"] << minio_endpoint
+  content_security_policies["frame-src"] << minio_endpoint
 end
 
 Decidim.configure do |config|
-  config.content_security_policies_extra = CSP
+  config.content_security_policies_extra = content_security_policies
 end
