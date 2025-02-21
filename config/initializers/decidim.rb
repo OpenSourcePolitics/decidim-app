6b26907d5e6a8c7945b98344988b2ffdfca78970
AyakorK <guillaume.moret@yahoo.com>
2025-02-21 16:01:28 +0100

    fix: Change the dynamic maps loading using OpenStreetMap

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -35,9 +35,25 @@ Decidim.configure do |config|
 
   # Geocoder configuration
   config.maps = {
+    # Keep HERE as the default provider for static maps
     provider: :here,
     api_key: Rails.application.secrets.maps[:api_key],
-    static: { url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview" },
+    static: {
+      url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview"
+    },
+
+    # Change to OSM for dynamic maps to avoid usage limits from HERE
+    dynamic: {
+      provider: :osm,
+      tile_layer: {
+        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        attribution: %(
+        &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors
+      )
+      }
+    },
+
+    # Keep HERE as the default provider for autocomplete
     autocomplete: {
       address_format: [%w(houseNumber street), "city", "country"]
     }
