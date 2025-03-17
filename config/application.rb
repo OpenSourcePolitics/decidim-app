998c0117efa57a8590dc13ceb33d840aacdf2173
stephanierousset <61418966+Stef-Rousset@users.noreply.github.com>
2025-03-17 09:29:37 +0100

    Backport/simple proposals custos (#667)
    * feat: add new config to proposals
    
    * feat: add proposal form extends
    
    * feat: update locales files
    
    * test: add tests
    
    * chore: add i18n files
    
    * style: update with rubocop

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DecidimApp
     config.load_defaults 7.0
 
     config.after_initialize do
+      require "extends/forms/decidim/proposals/proposal_form_extends"
       require "extends/controllers/decidim/admin/scopes_controller_extends"
       require "extends/controllers/decidim/scopes_controller_extends"
       require "extends/helpers/decidim/check_boxes_tree_helper_extends"
