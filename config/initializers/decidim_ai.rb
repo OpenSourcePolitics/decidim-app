c15c004d9fbc1b2f83a54f3a080e5089af9ff8ea
Quentin Champenois <quentin.champenois7@gmail.com>
2025-04-10 20:44:32 +0200

    feat(ai): Add Scaleway strategy

--- config/initializers/decidim_ai.rb
+++ config/initializers/decidim_ai.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 if Decidim.module_installed?(:ai)
+  opts = {}
   Decidim::Ai::Language.formatter = "Decidim::Ai::Language::Formatter"
 
   Decidim::Ai::SpamDetection.reporting_user_email = "your-admin@example.org"
-  Decidim::Ai::SpamDetection.resource_score_threshold = 0.75 # default
-  Decidim::Ai::SpamDetection.spam_detection_delay = 30.seconds # default
+  Decidim::Ai::SpamDetection.resource_score_threshold = 0.75
+  Decidim::Ai::SpamDetection.spam_detection_delay = 30.seconds
   Decidim::Ai::SpamDetection.resource_analyzers = [
     {
-      name: :bayes,
-      strategy: Decidim::Ai::SpamDetection::Strategy::Bayes,
-      options: {
-        adapter: ENV.fetch("DECIDIM_SPAM_DETECTION_BACKEND_RESOURCE", "redis"),
-        params: { url: ENV.fetch("DECIDIM_SPAM_DETECTION_BACKEND_RESOURCE_REDIS_URL", "redis://localhost:6379/2") }
-      }
+      name: :scaleway,
+      strategy: Decidim::Ai::SpamDetection::Strategy::Scaleway,
+      options: opts
     }
   ]
   Decidim::Ai::SpamDetection.resource_models = begin
@@ -30,17 +28,14 @@ if Decidim.module_installed?(:ai)
   Decidim::Ai::SpamDetection.user_score_threshold = 0.75 # default
   Decidim::Ai::SpamDetection.user_analyzers = [
     {
-      name: :bayes,
-      strategy: Decidim::Ai::SpamDetection::Strategy::Bayes,
-      options: {
-        adapter: ENV.fetch("DECIDIM_SPAM_DETECTION_BACKEND_USER", "redis"),
-        params: { url: ENV.fetch("DECIDIM_SPAM_DETECTION_BACKEND_USER_REDIS_URL", "redis://localhost:6379/3") }
-      }
+      name: :scaleway,
+      strategy: Decidim::Ai::SpamDetection::Strategy::Scaleway,
+      options: opts
     }
   ]
   Decidim::Ai::SpamDetection.user_models = {
     "Decidim::User" => "Decidim::Ai::SpamDetection::Resource::UserBaseEntity"
   }
-  Decidim::Ai::SpamDetection.user_detection_service = "Decidim::Ai::SpamDetection::Service"
-  Decidim::Ai::SpamDetection.resource_detection_service = "Decidim::Ai::SpamDetection::Service"
+  Decidim::Ai::SpamDetection.user_detection_service = "Decidim::Ai::ThirdPartyService"
+  Decidim::Ai::SpamDetection.resource_detection_service = "Decidim::Ai::ThirdPartyService"
 end
