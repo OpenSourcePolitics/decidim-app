2a145d259828529fd871bc12a34c9761c3aee9f9
Quentin Champenois <quentin.champenois7@gmail.com>
2025-04-28 16:16:37 +0200

    fix(db): Add missing colunms

--- db/migrate/20250123165735_remove_state_from_decidim_proposals_proposals.decidim_proposals.rb
+++ db/migrate/20250123165735_remove_state_from_decidim_proposals_proposals.decidim_proposals.rb
@@ -3,7 +3,9 @@
 # This migration comes from decidim_proposals (originally 20240110203503)
 class RemoveStateFromDecidimProposalsProposals < ActiveRecord::Migration[6.1]
   def up
-    rename_column :decidim_proposals_proposals, :state, :old_state
+    if column_exists?(:decidim_proposals_proposal_states, :state)
+      rename_column :decidim_proposals_proposals, :state, :old_state
+    end
   end
 
   def down
