61cf45396461f8f7cf5024b14b2137c424635db3
Quentin Champenois <quentin.champenois7@gmail.com>
2025-04-28 15:43:59 +0200

    fix(db): Ensure migrations rollback

--- db/migrate/20250123165731_add_link_to_decidim_attachments.decidim_participatory_processes.rb
+++ db/migrate/20250123165731_add_link_to_decidim_attachments.decidim_participatory_processes.rb
@@ -2,7 +2,7 @@
 
 # This migration comes from decidim_participatory_processes (originally 20240529161054)
 class AddLinkToDecidimAttachments < ActiveRecord::Migration[6.1]
-  def change
-    add_column :decidim_attachments, :link, :string
+  def up
+    add_column :decidim_attachments, :link, :string, if_not_exists: true
   end
 end
