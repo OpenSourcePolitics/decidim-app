bd2c77676058794ef40f93752e0e8c65765586ea
stephanie rousset <strabon@club-internet.fr>
2025-06-25 10:40:41 +0200

    feat: add extends

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module DecidimApp
       require "extends/controllers/decidim/participatory_processes/participatory_processes_controller_extends"
       # helpers
       require "extends/helpers/decidim/check_boxes_tree_helper_extends"
+      require "extends/helpers/decidim/omniauth_helper_extends"
       # cells
       require "extends/cells/decidim/system/system_checks_cell_extends"
       require "extends/cells/decidim/comments/comment_metadata_cell_extends"
