9f6300f583e3d894f93654176b8ef4a5396d7d74
stephanie rousset <strabon@club-internet.fr>
2025-08-29 09:59:34 +0200

    feat: add translations for message

--- spec/system/unregistered_user_survey_spec.rb
+++ spec/system/unregistered_user_survey_spec.rb
@@ -62,7 +62,7 @@ describe "Answer a survey" do
       expect(page).to have_i18n_content(questionnaire.title)
       expect(page).to have_i18n_content(questionnaire.description)
       # testing that non connected users can't have access to upload of files
-      expect(page).to have_content("Le téléchargement de documents n'est autorisé qu'aux utilisateurs connectés.")
+      expect(page).to have_content("Uploading files is only allowed for connected users.")
       expect(page).to have_button("Add file", disabled: true)
 
       fill_in question.body["en"], with: "My first answer"
