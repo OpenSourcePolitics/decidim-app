e5dc2b663b11c18ab05a6c1df287339692149642
stephanie rousset <strabon@club-internet.fr>
2025-09-30 11:25:14 +0200

    fix: remove escaping from categorie's names

--- lib/extends/helpers/decidim/check_boxes_tree_helper_extends.rb
+++ lib/extends/helpers/decidim/check_boxes_tree_helper_extends.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
 
 module CheckBoxesTreeHelperExtends
+
+  def filter_categories_values
+    sorted_main_categories = current_participatory_space.categories.first_class.includes(:subcategories).sort_by do |category|
+      [category.weight, translated_attribute(category.name)]
+    end
+
+    categories_values = sorted_main_categories.flat_map do |category|
+      sorted_descendant_categories = category.descendants.includes(:subcategories).sort_by do |subcategory|
+        [subcategory.weight, translated_attribute(subcategory.name)]
+      end
+
+      subcategories = sorted_descendant_categories.flat_map do |subcategory|
+        Decidim::CheckBoxesTreeHelper::TreePoint.new(subcategory.id.to_s, translated_attribute(subcategory.name))
+      end
+
+      Decidim::CheckBoxesTreeHelper::TreeNode.new(
+        Decidim::CheckBoxesTreeHelper::TreePoint.new(category.id.to_s, translated_attribute(category.name)),
+        subcategories
+      )
+    end
+
+    Decidim::CheckBoxesTreeHelper::TreeNode.new(
+      Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.core.application_helper.filter_category_values.all")),
+      categories_values
+    )
+  end
+
   def filter_scopes_values
     return filter_scopes_values_from_parent(current_component.scope) if current_component.scope.present?
 
