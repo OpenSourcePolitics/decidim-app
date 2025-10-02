58313229a528b43b2b2f0175a26e21079d8c122b
stephanie rousset <strabon@club-internet.fr>
2025-10-02 09:20:26 +0200

    fix: area's filter not displayed if there are no areas

--- lib/extends/helpers/decidim/check_boxes_tree_helper_extends.rb
+++ lib/extends/helpers/decidim/check_boxes_tree_helper_extends.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 module CheckBoxesTreeHelperExtends
+  def filter_areas_values
+    areas_or_types = areas_for_select(current_organization)
+
+    areas_values = if areas_or_types.first.is_a?(Decidim::Area)
+                     filter_areas(areas_or_types)
+                   else
+                     filter_areas_and_types(areas_or_types)
+                   end
+    return if areas_values.blank?
+
+    Decidim::CheckBoxesTreeHelper::TreeNode.new(
+      Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.core.application_helper.filter_area_values.all")),
+      areas_values
+    )
+  end
+
   def filter_scopes_values
     return filter_scopes_values_from_parent(current_component.scope) if current_component.scope.present?
 
@@ -37,6 +53,15 @@ module CheckBoxesTreeHelperExtends
     scopes_values.prepend(Decidim::CheckBoxesTreeHelper::TreePoint.new("global", t("decidim.scopes.global"))) if participatory_space&.scope.blank?
 
     filter_tree_from(scopes_values)
+  end
+
+  def filter_tree_from(scopes_values)
+    if scopes_values.present?
+      Decidim::CheckBoxesTreeHelper::TreeNode.new(
+        Decidim::CheckBoxesTreeHelper::TreePoint.new("", t("decidim.core.application_helper.filter_scope_values.all")),
+        scopes_values
+      )
+    end
   end
 
   def scope_children_to_tree(scope, participatory_space = nil)
