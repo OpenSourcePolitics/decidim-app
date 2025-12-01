891ac3daf58adb1a4af393199292d82aa0e833e0
barbara oliveira <barbarafictiovore@gmail.com>
2025-12-01 16:22:06 +0100

    change request: .exists? method instead of .any who load all admins...

--- config/initializers/instant_spam_mail_blocker.rb
+++ config/initializers/instant_spam_mail_blocker.rb
@@ -25,7 +25,7 @@ module Decidim
     end
 
     def frequency_notifications_is_realtime?(admins)
-      admins.any? { |a| a.notifications_sending_frequency == "realtime" }
+      admins.where(notifications_sending_frequency: "realtime").exists?
     end
   end
 end
