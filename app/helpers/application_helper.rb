cf7940303a18cf224619b2869b7e73cee67a0512
moustachu <moustachu@opensourcepolitics.eu>
2025-12-18 14:59:32 +0100

    fix: disable profile fields when omniauth profile sync is enabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@
 
 module ApplicationHelper
   def force_profile_sync_on_omniauth_connection?
-    !current_organization.sign_in_enabled? &&
-      current_organization.enabled_omniauth_providers.any? &&
+    current_organization.enabled_omniauth_providers.any? &&
+      session["omniauth.provider"].present? &&
       Rails.application.secrets.dig(:decidim, :omniauth, :force_profile_sync)
   end
 end
