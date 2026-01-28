6e176f75438db3b7b47127006413e9febd19b7de
stephanie rousset <strabon@club-internet.fr>
2026-01-28 15:14:26 +0100

    fix: replace nokogiri xml by nokogiri html

---
+++ lib/extends/lib/decidim/decidim_awesome/custom_fields_extends.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module CustomFieldsExtends
+  extend ActiveSupport::Concern
+
+  included do
+    private
+
+    def parse_xml(xml)
+      debugger
+      @xml = xml
+      @data = Nokogiri.HTML(@xml).xpath("//dl/dd")
+      return if @data.present?
+
+      apply_to_first_textarea
+    end
+  end
+end
+
+Decidim::DecidimAwesome::CustomFields.include(CustomFieldsExtends)
