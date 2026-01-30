4fe3d7c4083646fa3fec0997bae7dcfc4cee2f18
AyakorK <guillaume.moret@yahoo.com>
2026-01-30 17:16:56 +0100

    Squashed commit of the following:
    commit 8caf0aa6523f784bf1c4e2c50d14524f4826b23b
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Fri Jan 30 16:59:50 2026 +0100
    
        fix: jquery version and revert some changes
    
    commit d5f1c14d9295f7759ff4695448045b5c86113c91
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Fri Jan 30 11:25:15 2026 +0100
    
        fix: Increase to 8 parallel processors
    
    commit 8d2ab4bff81d4eeb161a0520d0e512b74637298f
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Fri Jan 30 11:10:16 2026 +0100
    
        fix: Add more steps to tests
    
    commit 3ac828bff1bc69d76f029e65f3a798c451873f12
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Fri Jan 30 10:57:58 2026 +0100
    
        fix: steps of CI
    
    commit 437f94d11473030ed361f3863db98998d27ffe55
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 19:05:28 2026 +0100
    
        fix: Try to fix cache_manifest in test env
    
    commit 9bfce11abe956965c864713f346f7c1405b711bc
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 18:16:35 2026 +0100
    
        fix: Revert last changes
    
    commit 85511d9034534a3d372760cd61a9e1af17057f6b
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 17:23:37 2026 +0100
    
        fix: Try to change shakapacker compilation state
    
    commit 502c2075ac368f247100305168e79217a8d4034e
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 16:15:27 2026 +0100
    
        fix: Try to split system specs to make them shorter
    
    commit 601d6cbec8d493b5fa0b15fd4cb909789ba37338
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 15:11:35 2026 +0100
    
        fix: try to cache some assets
    
    commit e7df850132f40d98636eba03b5b682cdf7b576f0
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 13:48:48 2026 +0100
    
        fix: Remove capabilities
    
    commit 45c9bccfce5cf33ba8a0158f0e0e35a22364e403
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 13:33:33 2026 +0100
    
        fix: Remove useless line
    
    commit b942d5ebf248a621f8c86f3995036863a2511b35
    Author: AyakorK <guillaume.moret@yahoo.com>
    Date:   Thu Jan 29 13:10:53 2026 +0100
    
        fix: Try to bump chromedriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,23 +8,75 @@ require "decidim/dev/test/base_spec_helper"
 RSpec.configure do |config|
   config.formatter = ENV.fetch("RSPEC_FORMAT", "progress").to_sym
 
+  Capybara.default_max_wait_time = ENV["CI"] ? 10 : 5
+  Capybara.disable_animation = true
+  Capybara.server = :puma, { Silent: true }
+
+  if ENV["CI"]
+    require "redis"
+    Redis.new
+    Capybara.server_port = 9887 + ENV["TEST_ENV_NUMBER"].to_i
+  end
+
   Capybara.register_driver :headless_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
+
     options.args << "--headless=new"
     options.args << "--no-sandbox"
     options.args << "--disable-dev-shm-usage"
     options.args << "--disable-search-engine-choice-screen"
+    options.args << "--disable-gpu"
+    options.args << "--disable-software-rasterizer"
+    options.args << "--disable-extensions"
+    options.args << "--disable-background-networking"
+    options.args << "--disable-default-apps"
+    options.args << "--disable-sync"
+    options.args << "--disable-translate"
+    options.args << "--disable-web-security"
+    options.args << "--hide-scrollbars"
+    options.args << "--metrics-recording-only"
+    options.args << "--mute-audio"
+    options.args << "--no-first-run"
+    options.args << "--safebrowsing-disable-auto-update"
+    options.args << "--disable-blink-features=AutomationControlled"
+    options.args << "--force-prefers-reduced-motion"
+    options.args << "--user-data-dir=/tmp/decidim_tests_user_data_#{ENV.fetch("TEST_ENV_NUMBER", nil)}_#{rand(1000)}"
     options.args << if ENV["BIG_SCREEN_SIZE"].present?
                       "--window-size=1920,3000"
                     else
                       "--window-size=1920,1080"
                     end
     options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
+
     options.add_preference("intl.accept_languages", "en-GB")
+    options.add_preference("download.prompt_for_download", false)
+    options.add_preference("download.default_directory", "/tmp")
+
+    options.logging_prefs = { browser: "OFF", driver: "OFF" } if ENV["CI"]
+
     Capybara::Selenium::Driver.new(
       app,
       browser: :chrome,
-      options:
+      options: options,
+      timeout: 180
     )
+  end
+
+  config.before(:each, type: :system) do
+    driven_by :headless_chrome
+
+    Capybara.current_session.server.wait_for_pending_requests if ENV["CI"] && Capybara.current_session.server
+  end
+
+  config.after(:each, type: :system) do
+    if page.driver.browser.respond_to?(:window_handles)
+      page.driver.browser.window_handles[1..-1]&.each do |handle|
+        page.driver.browser.switch_to.window(handle)
+        page.driver.browser.close
+      end
+      page.driver.browser.switch_to.window(page.driver.browser.window_handles.first) if page.driver.browser.window_handles.any?
+    end
+  rescue Selenium::WebDriver::Error::InvalidSessionIdError
+    # Session already closed, nothing to do here
   end
 end
