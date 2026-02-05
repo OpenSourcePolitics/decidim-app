c145466e53e0774012c2682b83a2c0eeffb1652e
moustachu <moustachu@opensourcepolitics.eu>
2026-02-05 16:45:56 +0100

    fix: (v1.2.0) fully disable Rack Attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,4 +6,5 @@ require "decidim_app/rack_attack/fail2ban"
 
 # Enabled by default in production
 # Can be deactivated with 'ENABLE_RACK_ATTACK=0'
-DecidimApp::RackAttack.apply_configuration if DecidimApp::RackAttack.rack_enabled?
+# DecidimApp::RackAttack.apply_configuration if DecidimApp::RackAttack.rack_enabled?
+Rack::Attack.enabled = false
