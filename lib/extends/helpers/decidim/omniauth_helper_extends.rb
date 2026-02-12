98d74bd11a57a60ad42f13c989dd97091375ed15
stephanie rousset <strabon@club-internet.fr>
2026-02-12 15:08:24 +0100

    fix: udpate default partial to display custom publik translation

--- lib/extends/helpers/decidim/omniauth_helper_extends.rb
+++ lib/extends/helpers/decidim/omniauth_helper_extends.rb
@@ -24,11 +24,15 @@ module OmniauthHelperExtends
   def normalize_provider_name(provider)
     return "x" if provider == :twitter
     # customize the name of the omniauth btn login with publik
-    if provider == :publik && Decidim::TermCustomizer::Translation.where(key: "decidim.devise.shared.links.log_in_with_provider").present?
+    if custom_publik_translation?(provider)
       return I18n.t("decidim.devise.shared.links.log_in_with_provider")
     end
 
     provider.to_s.split("_").first
+  end
+
+  def custom_publik_translation?(provider)
+    provider == :publik && Decidim::TermCustomizer::Translation.where(key: "decidim.devise.shared.links.log_in_with_provider").present?
   end
 end
 
