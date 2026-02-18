072662aebc2463cd34f4249ac09ec3223bfd4af1
stephanierousset <61418966+Stef-Rousset@users.noreply.github.com>
2026-02-18 09:27:26 +0100

    fix: update default partial to display custom publik translation (#944)
    * fix: udpate default partial to display custom publik translation
    
    * style: update with rubocop

--- lib/extends/helpers/decidim/omniauth_helper_extends.rb
+++ lib/extends/helpers/decidim/omniauth_helper_extends.rb
@@ -24,11 +24,13 @@ module OmniauthHelperExtends
   def normalize_provider_name(provider)
     return "x" if provider == :twitter
     # customize the name of the omniauth btn login with publik
-    if provider == :publik && Decidim::TermCustomizer::Translation.where(key: "decidim.devise.shared.links.log_in_with_provider").present?
-      return I18n.t("decidim.devise.shared.links.log_in_with_provider")
-    end
+    return I18n.t("decidim.devise.shared.links.log_in_with_provider") if custom_publik_translation?(provider)
 
     provider.to_s.split("_").first
+  end
+
+  def custom_publik_translation?(provider)
+    provider == :publik && Decidim::TermCustomizer::Translation.where(key: "decidim.devise.shared.links.log_in_with_provider").present?
   end
 end
 
