946e6164bfdde4522a6ff620a5c420a813ee9fd2
stephanierousset <61418966+Stef-Rousset@users.noreply.github.com>
2026-03-02 13:38:56 +0100

    fix: special characters in editor (#947)
    * fix: add validation on editor image form to avoid special characters
    
    * fix: add javascript to handle errors on editor form
    
    * test: add tests

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module DecidimApp
       require "extends/forms/decidim/comments/comment_form_extends"
       require "extends/forms/decidim/system/base_organization_form_extends"
       require "extends/forms/decidim/omniauth_registration_form_extends"
+      require "extends/forms/decidim/editor_image_form_extends"
       # controllers
       require "extends/controllers/decidim/admin/scopes_controller_extends"
       require "extends/controllers/decidim/scopes_controller_extends"
---
+++ spec/forms/editor_image_form_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  describe EditorImageForm do
+    subject { described_class.from_params(attributes).with_context(context) }
+
+    let(:attributes) do
+      {
+        "editor_image" => {
+          organization:,
+          author_id: user_id,
+          file:
+        }
+      }
+    end
+    let(:context) do
+      {
+        current_organization: organization,
+        current_user: user
+      }
+    end
+    let(:user) { create(:user, :admin, :confirmed) }
+    let(:organization) { user.organization }
+    let(:user_id) { user.id }
+    let(:file) { Decidim::Dev.test_file("city.jpeg", "image/jpeg") }
+
+    context "with correct data" do
+      it "is valid" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "with an empty user_id" do
+      let(:user_id) { nil }
+
+      it "is invalid" do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context "with an empty organization" do
+      let(:organization) { nil }
+
+      it "is invalid" do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context "when images are not the expected type" do
+      let(:file) { Decidim::Dev.test_file("Exampledocument.pdf", "application/pdf") }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when the original filename of the image has an underscore" do
+      let(:file) do
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("app/packs/images/FranceConnect-Bouton/01-Principal/png/franceconnect-btn-principal.png"),
+          "image/png",
+          original_filename: "test_image.jpg"
+        )
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when the original filename of the image has a dash" do
+      let(:file) do
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("app/packs/images/FranceConnect-Bouton/01-Principal/png/franceconnect-btn-principal.png"),
+          "image/png",
+          original_filename: "test-image.jpg"
+        )
+      end
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when the original filename of the image has an accent" do
+      let(:file) do
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("app/packs/images/FranceConnect-Bouton/01-Principal/png/franceconnect-btn-principal.png"),
+          "image/png",
+          original_filename: "imàge.jpg"
+        )
+      end
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when the original filename of the image has a space" do
+      let(:file) do
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("app/packs/images/FranceConnect-Bouton/01-Principal/png/franceconnect-btn-principal.png"),
+          "image/png",
+          original_filename: "test image.jpg"
+        )
+      end
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when the original filename of the image has a parenthesis" do
+      let(:file) do
+        Rack::Test::UploadedFile.new(
+          Rails.root.join("app/packs/images/FranceConnect-Bouton/01-Principal/png/franceconnect-btn-principal.png"),
+          "image/png",
+          original_filename: "test(image.jpg"
+        )
+      end
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+end
